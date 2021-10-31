8c7afc1c5962c3bd5a80a3a70ba50392731ba11f
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2021-11-01 02:05:25 +0530

    Adding Skill route

---
+++ components/Skill.js
@@ -0,0 +1,29 @@
+import Image from "next/image";
+
+function Skill({ data }) {
+  return (
+    <div className="max-w-sm flex items-center justify-center px-4 py-3 shadow rounded-lg">
+      <div className="image">
+        <Image src={`/${data.icon}`} alt={data.name} width={60} height={60} />
+      </div>
+      <div className="flex flex-col pl-3 justify-evenly mx-auto my-0 capitalize w-full ">
+        <div className="w-full flex justify-between mb-2 text-xl">
+          <p className="name">{data.name}</p>
+          <p className="font-bold">{data.level ? data.level : "0%"}</p>
+        </div>
+        <div className="relative">
+          <div
+            style={{
+              maxWidth: "100%",
+              width: data.level ? `${data.level}` : "10%",
+            }}
+            className="skill__bar absolute text-center h-[10px] rounded-xl z-10 bg-gray-900"
+          ></div>
+          <div className="w-ull bg-gray-200 h-[10px] z-0 rounded-xl"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Skill;
--- pages/skills.js
+++ pages/skills.js
@@ -1,7 +1,27 @@
-export default function Skills() {
+import Skill from "../components/Skill";
+
+export default function Skills({ skills }) {
   return (
-    <div>
-      skills
-    </div>
-  )
+    <>
+      <h3 className="title_of_page">Skills</h3>
+
+      <section className="page_container">
+        {skills.map((skill) => {
+          return <Skill key={skill.id} data={skill} />;
+        })}
+      </section>
+    </>
+  );
+}
+
+export async function getStaticProps() {
+  const res = await fetch(process.env.NEXT_PUBLIC_API_URL + "/skills");
+  const skills = await res.json();
+  console.log(skills);
+
+  return {
+    props: {
+      skills,
+    },
+  };
 }
