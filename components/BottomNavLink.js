89eb98a04e0a1bab635e89c088b69fa3180f1a2f
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2021-11-13 15:21:37 +0530

    bottom navbar new styles applied

---
+++ components/BottomNavLink.js
@@ -0,0 +1,22 @@
+import Link from "next/link";
+
+export default function BottomNavLink({ Icon, info, href }) {
+  return (
+    <Link href={href}>
+      <div
+        className={`relative flex flex-col justify-center items-center ${
+          info.active && "text-purple-600 w-auto animate-wiggle"
+        }`}
+      >
+        <Icon
+          className={`bottom_nav_icon ${
+            info.active && " transform duration-150"
+          }`}
+        />
+        <p className="text-[10px] left-0 right-0  font-semibold text-center -bottom-1 uppercase">
+          {info.title}
+        </p>
+      </div>
+    </Link>
+  );
+}
