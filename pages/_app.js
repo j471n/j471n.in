3e6488a4bb0c290e0f590b7109ffb81f10690ee2
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2021-12-07 12:21:44 +0530

    Adding Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import { useEffect } from "react";
 import { useRouter } from "next/router";
 import NProgress from "nprogress";
 import "nprogress/nprogress.css";
+import Script from "next/script";
 
 NProgress.configure({
   easing: "ease",
@@ -13,13 +14,12 @@ NProgress.configure({
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
+
   useEffect(() => {
     const start = () => {
-      // console.log("start");
       NProgress.start();
     };
     const end = () => {
-      // console.log("finished");
       NProgress.done();
     };
     router.events.on("routeChangeStart", start);
@@ -33,9 +33,34 @@ function MyApp({ Component, pageProps }) {
   }, []);
 
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      {process.env.NODE_ENV === "production" && (
+        <>
+          <Script
+            strategy="lazyOnload"
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
+          />
+          <Script
+            id="gtag-init"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}', {
+                  page_path: window.location.pathname,
+                  page_location : window.location.href,
+                });
+            `,
+            }}
+          />
+        </>
+      )}
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
 
