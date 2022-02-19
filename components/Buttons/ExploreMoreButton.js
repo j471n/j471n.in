7001ed31d84e822f23908d5f4bcebae7014e1b89
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2022-02-20 00:50:12 +0530

    Adding snap-x

--- components/Buttons/ExploreMoreButton.js
+++ components/Buttons/ExploreMoreButton.js
@@ -2,7 +2,7 @@ import { FiArrowRightCircle } from "react-icons/fi";
 
 export default function ExploreMoreButton({ link }) {
   return (
-    <div className="flex flex-col group items-center self-center relative">
+    <div className="flex flex-col group items-center self-center relative snap-center">
       <div
         className="flex ring hover:ring-purple-300 rounded-full m-3 cursor-pointer clickable_button relative"
         onClick={() => window.open(link, "_self")}
