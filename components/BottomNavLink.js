e94c986ef655dc00e9419067fea9c23ce7532a65
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2022-02-21 11:17:31 +0530

    Updating the style of bottom navigation

--- components/BottomNavLink.js
+++ components/BottomNavLink.js
@@ -4,18 +4,14 @@ export default function BottomNavLink({ Icon, info, href }) {
   return (
     <Link href={href}>
       <div
-        className={`relative flex flex-col ${
+        className={`relative flex flex-col transition-all duration-300 ${
           info.active ? "dark:text-purple-400" : "dark:text-gray-400"
         } justify-center items-center ${
           info.active && "text-purple-600 w-auto animate-wiggle"
         }`}
       >
-        <Icon
-          className={`bottom_nav_icon ${
-            info.active && " transform duration-150"
-          }`}
-        />
-        <p className="text-[10px] left-0 right-0  font-semibold text-center -bottom-1 uppercase">
+        <Icon className="bottom_nav_icon" />
+        <p className="text-[10px] font-semibold text-center uppercase">
           {info.title}
         </p>
       </div>
