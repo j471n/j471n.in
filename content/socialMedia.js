cbf54f540d545b4de7fbecfc81d4cf2037e3181f
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2022-02-24 04:58:43 +0530

    Updating the UI of Contact Page

--- content/socialMedia.js
+++ content/socialMedia.js
@@ -1,10 +1,10 @@
 export default {
   instagram: "https://www.instagram.com/jatin47.47/",
-  twitter: "https://twitter.com/jatin47_",
+  twitter: "https://twitter.com/j471n_",
   codepen: "https://codepen.io/j471n",
   devTo: "https://dev.to/j471n",
   facebook: "https://www.facebook.com/ja7in/",
-  linkedIn: "www.linkedin.com/in/j471n",
+  linkedIn: "https://www.linkedin.com/in/j471n/",
   github: "https://github.com/j471n",
   mailto: "mailto:jatinsharma8669@gmail.com",
 };
--- pages/contact.js
+++ pages/contact.js
@@ -3,15 +3,16 @@ import React, { useState } from "react";
 import emailjs from "emailjs-com";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { AiOutlineInstagram, AiOutlineTwitter } from "react-icons/ai";
+import {
+  AiOutlineInstagram,
+  AiOutlineTwitter,
+  AiOutlineLoading,
+} from "react-icons/ai";
 import { BsFacebook, BsGithub, BsLinkedin } from "react-icons/bs";
 import { FaDev } from "react-icons/fa";
 import { HiMail } from "react-icons/hi";
 import { SiCodepen } from "react-icons/si";
 import SocialIcon from "../components/SocialIcon";
-// import socialMedia from "../content/socialMedia";
-import Loading from "../components/Loading";
-import CoverPage from "../components/CoverPage";
 import { getSocialMedia } from "../lib/dataFetch";
 import Metadata from "../components/MetaData";
 
@@ -30,7 +31,6 @@ export default function Contact({ socialMedia }) {
 
   function sendEmail(e) {
     e.preventDefault();
-    toast.success("Message Sent ✌");
 
     setLoading(true);
     emailjs
@@ -54,166 +54,196 @@ export default function Contact({ socialMedia }) {
   }
 
   return (
-    <>
+    <div className="min-h-screen dark:bg-darkSecondary">
       <Metadata title="Contact 🤙" />
 
-      <CoverPage
-        title="Want to"
-        mainHeading="Contact me"
-        className="grid place-items-center"
-      />
+      {/* Get in touch top section */}
+      <section className="w-full-width text-center pt-6 dark:bg-darkSecondary dark:text-white">
+        <h1 className="font-bold text-4xl">Get in touch</h1>
+        <p className="px-4 py-2 font-medium dark:text-gray-300">
+          Have a little something, something you wanna talk about? Please feel
+          free to get in touch anytime, whether for work or to just Hi 🙋‍♂️.
+        </p>
+      </section>
 
-      <section className="w-full flex flex-col items-center p-4 pb-24 dark:bg-darkPrimary">
-        {/* <h3 className="title_of_page">Contact Me!</h3> */}
-        {loading ? (
-          <Loading />
-        ) : (
-          <React.Fragment>
-            <div className="p-2 m-4 w-24 h-24 shadow rounded-full cursor-pointer lg:hover:animate-bounce">
-              <Image
-                className="w-full h-full rounded-full"
-                src="/img/profile.jpg"
-                alt="Jatin Sharma"
-                loading="lazy"
-                width={96}
-                height={96}
-              />
-            </div>
+      {/* Wrapper Container */}
+      <section className="flex flex-col lg:flex-row w-full mx-auto px-5 dark:bg-darkSecondary dark:text-white">
+        {/* Left Contact form section */}
+        <div className="w-full mx-auto mt-10">
+          <h2 className="text-2xl font-bold w-full text-center my-2">
+            Connect with me
+          </h2>
 
-            <form
-              className="w-full flex flex-col items-center max-w-sm dark:text-gray-300"
-              onSubmit={sendEmail}
+          <form
+            className="w-full flex flex-col items-center max-w-sm mx-auto dark:text-gray-300"
+            onSubmit={sendEmail}
+          >
+            <input
+              className="contact_field"
+              value={emailInfo.from_name}
+              type="text"
+              name="from_name"
+              placeholder="Name"
+              required
+              onChange={(e) =>
+                setEmailInfo({
+                  ...emailInfo,
+                  [e.target.name]: e.target.value,
+                })
+              }
+            />
+            <input
+              className="contact_field"
+              value={emailInfo.email}
+              type="email"
+              name="email"
+              placeholder="example@gmail.com"
+              required
+              onChange={(e) =>
+                setEmailInfo({
+                  ...emailInfo,
+                  [e.target.name]: e.target.value,
+                })
+              }
+            />
+            <input
+              className="contact_field"
+              value={emailInfo.subject}
+              type="text"
+              name="subject"
+              placeholder="Subject"
+              required
+              onChange={(e) =>
+                setEmailInfo({
+                  ...emailInfo,
+                  [e.target.name]: e.target.value,
+                })
+              }
+            />
+            <textarea
+              className="contact_field custom-scrollbar h-44  !resize-none md:resize"
+              name="message"
+              value={emailInfo.message}
+              placeholder="Message"
+              required
+              onChange={(e) =>
+                setEmailInfo({
+                  ...emailInfo,
+                  [e.target.name]: e.target.value,
+                })
+              }
+            ></textarea>
+            <button
+              className="w-full max-w-sm p-3 border-none rounded-md bg-purple-400 dark:text-darkPrimary font-semibold mt-4 cursor-pointer transform duration-150 active:scale-95 disabled:opacity-50 disabled:transform-none disabled:cursor-wait"
+              type="submit"
+              disabled={loading}
             >
-              <input
-                className="contact_field "
-                value={emailInfo.from_name}
-                type="text"
-                name="from_name"
-                placeholder="Name"
-                required
-                onChange={(e) =>
-                  setEmailInfo({
-                    ...emailInfo,
-                    [e.target.name]: e.target.value,
-                  })
-                }
-              />
-              <input
-                className="contact_field"
-                value={emailInfo.email}
-                type="email"
-                name="email"
-                placeholder="example@gmail.com"
-                required
-                onChange={(e) =>
-                  setEmailInfo({
-                    ...emailInfo,
-                    [e.target.name]: e.target.value,
-                  })
-                }
-              />
-              <input
-                className="contact_field"
-                value={emailInfo.subject}
-                type="text"
-                name="subject"
-                placeholder="Subject"
-                required
-                onChange={(e) =>
-                  setEmailInfo({
-                    ...emailInfo,
-                    [e.target.name]: e.target.value,
-                  })
-                }
-              />
-              <textarea
-                className="contact_field custom-scrollbar min-h-[120px] resize-none md:resize"
-                name="message"
-                value={emailInfo.message}
-                placeholder="Message"
-                required
-                onChange={(e) =>
-                  setEmailInfo({
-                    ...emailInfo,
-                    [e.target.name]: e.target.value,
-                  })
-                }
-              ></textarea>
-              <input
-                className="w-full max-w-sm p-3 border-none rounded-md bg-purple-400 dark:text-darkPrimary font-semibold mt-4 cursor-pointer  transform duration-150 active:scale-95"
-                type="submit"
-                value="Send"
-              />
-            </form>
-
-            <div className="p-0 mt-8 flex flex-col items-center">
-              <h3 className="text-xl text-center font-semibold dark:text-gray-100">
-                Social Media
-              </h3>
-
-              <div className="flex flex-wrap gap-2 sm:gap-4 items-center justify-center pt-4 dark:text-gray-100">
-                <SocialIcon
-                  Icon={AiOutlineInstagram}
-                  title="Instagram"
-                  url={socialMedia?.instagram}
-                />
-                <SocialIcon
-                  Icon={AiOutlineTwitter}
-                  title="Twitter"
-                  url={socialMedia.twitter}
-                />
-                <SocialIcon
-                  Icon={BsFacebook}
-                  title="Facebook"
-                  url={socialMedia.facebook}
-                />
-                <SocialIcon
-                  Icon={BsGithub}
-                  title="Github"
-                  url={socialMedia.github}
-                />
-                <SocialIcon
-                  Icon={BsLinkedin}
-                  title="LinkedIn"
-                  url={socialMedia.linkedIn}
-                />
-                <SocialIcon
-                  Icon={HiMail}
-                  title="Instagram"
-                  url={socialMedia.mailto}
-                />
-                <SocialIcon
-                  Icon={SiCodepen}
-                  title="Codepen"
-                  url={socialMedia.codepen}
-                />
-                <SocialIcon
-                  Icon={FaDev}
-                  title="Instagram"
-                  url={socialMedia.devTo}
-                />
+              <div className="relative w-full flex items-center justify-center">
+                <p
+                  className={
+                    loading ? "inline-flex animate-spin mr-3" : "hidden"
+                  }
+                >
+                  <AiOutlineLoading className="font-bold text-2xl" />
+                </p>
+                <p>{loading ? "Sending..." : "Send"}</p>
               </div>
-            </div>
+            </button>
+          </form>
+        </div>
+        {/* Right Other contact methods */}
+        <div className="w-full mx-auto mt-10 lg:border-l-4 lg:dark:border-zinc-400 flex flex-col justify-center items-center">
+          <h2 className="text-lg sm:text-2xl font-bold my-2">
+            Connect with me on Social Media
+          </h2>
 
-            <div className="buy-me-a-coffee m-4">
+          {/* Social Media Container */}
+          <div className="flex flex-wrap gap-2 sm:gap-4 items-center py-4 dark:text-gray-100">
+            <SocialIcon
+              Icon={AiOutlineInstagram}
+              title="Instagram"
+              url={socialMedia?.instagram}
+            />
+            <SocialIcon
+              Icon={AiOutlineTwitter}
+              title="Twitter"
+              url={socialMedia.twitter}
+            />
+            <SocialIcon
+              Icon={BsFacebook}
+              title="Facebook"
+              url={socialMedia.facebook}
+            />
+            <SocialIcon
+              Icon={BsGithub}
+              title="Github"
+              url={socialMedia.github}
+            />
+            <SocialIcon
+              Icon={BsLinkedin}
+              title="LinkedIn"
+              url={socialMedia.linkedIn}
+            />
+            <SocialIcon
+              Icon={HiMail}
+              title="Instagram"
+              url={socialMedia.mailto}
+            />
+            <SocialIcon
+              Icon={SiCodepen}
+              title="Codepen"
+              url={socialMedia.codepen}
+            />
+            <SocialIcon
+              Icon={FaDev}
+              title="Instagram"
+              url={socialMedia.devTo}
+            />
+          </div>
+          {/* Support Me */}
+          <div className="pb-24 lg:pb-0">
+            <h2 className="text-lg sm:text-2xl font-bold my-2">
+              Support me 💪
+            </h2>
+
+            <div className="flex items-center justify-center space-x-4 my-6">
+              {/* buy me a coffee */}
               <a
                 href="https://www.buymeacoffee.com/j471n"
                 target="_blank"
+                title="Buy me a coffee"
                 rel="noopener noreferrer"
               >
                 <Image
-                  src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png"
+                  src="https://imgur.com/a7OZ4EV.png"
                   alt="Buy Me A Coffee"
-                  width={217}
+                  width={60}
+                  className="rounded-full"
+                  height={60}
+                />
+              </a>
+
+              {/* PayPal Link */}
+              <a
+                href="https://www.paypal.com/paypalme/j47in"
+                target="_blank"
+                title="PayPal"
+                rel="noopener noreferrer"
+              >
+                <Image
+                  src="https://imgur.com/jFdBFwG.png"
+                  alt="PayPal"
+                  className="rounded-full"
+                  width={60}
                   height={60}
                 />
               </a>
             </div>
-          </React.Fragment>
-        )}
-        <ToastContainer theme="dark" />
+          </div>
+        </div>
       </section>
-    </>
+      <ToastContainer theme="dark" />
+    </div>
   );
 }
 
