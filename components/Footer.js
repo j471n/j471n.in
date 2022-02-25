969242095cbaaf53fe1d8c329623d18e8c7f0d75
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2022-02-25 14:36:20 +0530

    Updating <a/> to Link(next/link)

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
+import Link from "next/link";
 import socialMedia from "../content/socialMedia";
-import { AiOutlineTwitter } from "react-icons/ai";
-import { BsGithub, BsLinkedin, BsTwitter } from "react-icons/bs";
+import { BsGithub, BsTwitter } from "react-icons/bs";
 import { FiLinkedin } from "react-icons/fi";
 import SocialIcon from "../components/SocialIcon";
 export default function Footer() {
@@ -19,58 +19,44 @@ export default function Footer() {
           </div>
           <ul className="flex flex-wrap items-center space-x-4 sm:space-x-8">
             <li className="font-bold lg:hover:underline text-xs sm:text-base text-gray-500">
-              <a rel="noopener noreferrer" href="/">
+              <Link rel="noopener noreferrer" href="/">
                 Jatin Sharma
-              </a>
+              </Link>
             </li>
           </ul>
         </div>
 
         <p className="text-center text-gray-500 font-medium text-sm sm:text-base col-span-2 md:col-span-1 row-start-2 md:row-start-auto">
           &copy; {new Date().getFullYear()},{" "}
-          <a className="select-none hover:text-black dark:hover:text-white" rel="noopener noreferrer" href="/">
+          <Link className="select-none hover:text-black dark:hover:text-white" rel="noopener noreferrer" href="/">
             Jatin Sharma
-          </a>
+          </Link>
         </p>
 
         <ul className="flex flex-wrap space-x-4 !m-0 sm:space-x-7">
           <li>
-            <a rel="noopener noreferrer" href="#">
               <SocialIcon
                 Icon={BsTwitter}
                 title="Twitter"
                 url={socialMedia.twitter}
               />
-            </a>
           </li>
           <li>
-            <a rel="noopener noreferrer" href="#">
-              <SocialIcon
-                Icon={BsGithub}
-                title="Github"
-                url={socialMedia.github}
-              />
-            </a>
+            <SocialIcon
+              Icon={BsGithub}
+              title="Github"
+              url={socialMedia.github}
+            />
           </li>
           <li>
-            <a rel="noopener noreferrer" href="#">
-              <SocialIcon
-                Icon={FiLinkedin}
-                title="LinkedIn"
-                url={socialMedia.linkedIn}
-              />
-            </a>
+            <SocialIcon
+              Icon={FiLinkedin}
+              title="LinkedIn"
+              url={socialMedia.linkedIn}
+            />
           </li>
         </ul>
       </div>
     </footer>
   );
 }
-
-{
-  /* <footer className="w-full relative p-4 dark:bg-darkPrimary dark:text-gray-100 pb-24 -mt-24 sm:-mt-10 font-medium text-xs sm:text-base text-center z-40 print:hidden">
-      <p>Made with ❤️ by Jatin Sharma</p>
-    </footer> */
-}
-
-// flex items-center justify-between
