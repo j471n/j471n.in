66781a5bc3a60f367f5834b53b314119d58d21f9
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2022-03-30 01:22:23 +0530

    FIX: changing class to className

--- components/FAQ/Faq.js
+++ components/FAQ/Faq.js
@@ -2,13 +2,13 @@ import QuestionAndAnswer from "./QuestionAndAnswer";
 
 function FAQ({ faqs = [] }) {
   return (
-    <section class="bg-white dark:bg-darkPrimary">
-      <div class="px-8 md:px-36 py-8 mx-auto">
-        <h1 class="text-2xl font-bold text-center sm:text-left lg:text-center text-gray-800 lg:text-4xl dark:text-white">
+    <section className="bg-white dark:bg-darkPrimary">
+      <div className="px-8 md:px-36 py-8 mx-auto">
+        <h1 className="text-2xl font-bold text-center sm:text-left lg:text-center text-gray-800 lg:text-4xl dark:text-white">
           Frequently asked questions
         </h1>
 
-        <div class="grid grid-cols-1 gap-8 mt-8 lg:mt-16 md:grid-cols-2 xl:grid-cols-3">
+        <div className="grid grid-cols-1 gap-8 mt-8 lg:mt-16 md:grid-cols-2 xl:grid-cols-3">
           {faqs.map((faq) => {
             return (
               <QuestionAndAnswer
--- components/FAQ/QuestionAndAnswer.js
+++ components/FAQ/QuestionAndAnswer.js
@@ -2,29 +2,31 @@ export default function QuestionAndAnswer({ question, answer }) {
   if (!question || !answer) return null;
   return (
     <div>
-      <div class="inline-block p-2 sm:p-3 text-white bg-purple-700 rounded-lg">
+      <div className="inline-block p-2 sm:p-3 text-white bg-purple-700 rounded-lg">
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          class="w-6 h-6"
+          className="w-6 h-6"
           fill="none"
           viewBox="0 0 24 24"
           stroke="currentColor"
         >
           <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
             d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
           />
         </svg>
       </div>
 
       <div>
-        <h1 class="text-base sm:text-lg font-semibold text-gray-600 dark:text-slate-200">
+        <h1 className="text-base sm:text-lg font-semibold text-gray-600 dark:text-slate-200">
           {question}
         </h1>
 
-        <p class="mt-2 text-sm text-gray-400 dark:text-slate-400">{answer}</p>
+        <p className="mt-2 text-sm text-gray-400 dark:text-slate-400">
+          {answer}
+        </p>
       </div>
     </div>
   );
