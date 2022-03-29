a5e15050f4262f7235739d2b5f7c25e4a4d43579
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2022-03-30 00:51:55 +0530

    Updating Bottom Navigation bar

--- components/BottomNavLink.js
+++ components/BottomNavLink.js
@@ -1,18 +1,16 @@
 import Link from "next/link";
 
-export default function BottomNavLink({ Icon, info, href }) {
+export default function BottomNavLink({ Icon, active, href, title }) {
   return (
     <Link href={href}>
       <div
-        className={`relative flex flex-col transition-all duration-300 ${
-          info.active ? "dark:text-purple-400" : "dark:text-gray-400"
-        } justify-center items-center ${
-          info.active && "text-purple-600 w-auto animate-wiggle"
-        }`}
+        className={`relative flex flex-col flex-grow transition-all duration-300 ${
+          active ? "dark:text-purple-400" : "dark:text-gray-400"
+        } justify-center items-center ${active && "text-purple-600 w-auto"}`}
       >
         <Icon className="bottom_nav_icon" />
         <p className="text-[10px] font-semibold text-center uppercase">
-          {info.title}
+          {title}
         </p>
       </div>
     </Link>
