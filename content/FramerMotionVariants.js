d38cd8e2d91d0ac3dd1c6a4c8afb387349775135
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2022-04-01 13:59:31 +0530

    Adding More variants for animation

--- content/FramerMotionVariants.js
+++ content/FramerMotionVariants.js
@@ -6,8 +6,8 @@ export const popUp = {
     scale: 1,
   },
   transition: {
+    type: "spring",
     duration: 1,
-    ease: "easeInOut",
   },
 };
 
@@ -43,13 +43,88 @@ export const slideFromLeft = {
 
 export const popUpFromBottomForText = {
   hidden: { opacity: 0, y: 10 },
-  visible: { opacity: 1, y: 0 },
-  transition: {
-    duration : "2s"
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      delay: 0.1,
+    },
   },
 };
 
 export const pulseOpacity = {
   hidden: { opacity: 0 },
   visible: { opacity: 1 },
+};
+
+export const headingFromLeft = {
+  hidden: { x: -200, opacity: 0 },
+  visible: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.1,
+      type: "spring",
+      stiffness: 70,
+    },
+  },
+};
+
+export const inputSlideAnimation = {
+  hidden: { x: -200, opacity: 0 },
+  visible: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      // duration: 0.1,
+      type: "spring",
+      // stiffness: 70,
+    },
+  },
+};
+
+export const fromBottomVariant = {
+  hidden: { y: 200, opacity: 0 },
+  visible: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.1,
+      type: "spring",
+      stiffness: 100,
+    },
+  },
+};
+
+export const cardFromRight = {
+  hidden: { x: 100, opacity: 1 },
+  visible: {
+    x: 0,
+    opacity: 1,
+    // scale: 1,
+    transition: {
+      // duration: 0.1,
+      type: "spring",
+      // stiffness: 100,
+    },
+  },
+};
+
+export const homeEnterVariant = {
+  hidden: { x: "100vw", opacity: 1 },
+  visible: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      type: "spring",
+      mass: 0.4,
+      damping: 8,
+      when: "beforeChildren",
+      staggerChildren: 0.4,
+    },
+  },
+  exit: {
+    x: "-100vw",
+    opacity: 1,
+  },
 };
