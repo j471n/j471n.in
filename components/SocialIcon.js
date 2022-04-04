a0765ff26667dfa3c1307b9c385531792771c5f3
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2022-04-04 12:36:37 +0530

    Removing Motion

--- components/SocialIcon.js
+++ components/SocialIcon.js
@@ -1,34 +1,34 @@
 // import Link from "next/link";
-import { motion, useAnimation } from "framer-motion";
-import { useInView } from "react-intersection-observer";
-import { fromRightVariant } from "../content/FramerMotionVariants";
-import { useEffect } from "react";
+// import { motion, useAnimation } from "framer-motion";
+// import { useInView } from "react-intersection-observer";
+// import { cardFromRight, fromBottomVariant, fromLeftVariant, fromRightVariant } from "../content/FramerMotionVariants";
+// import { useEffect } from "react";
 
 export default function SocialIcon({ Icon, title, url }) {
-  const controls = useAnimation();
-  const [ref, inView] = useInView({
-    threshold: 0.4,
-  });
+  // const controls = useAnimation();
+  // const [ref, inView] = useInView({
+  //   threshold: 0.4,
+  // });
 
-  useEffect(() => {
-    if (inView) {
-      controls.start("visible");
-    } else {
-      controls.start("hidden");
-    }
-  }, [inView]);
+  // useEffect(() => {
+  //   if (inView) {
+  //     controls.start("visible");
+  //   } else {
+  //     controls.start("hidden");
+  //   }
+  // }, [inView]);
 
   return (
-    <motion.a
-      ref={ref}
-      initial="hidden"
-      animate={controls}
-      variants={fromRightVariant}
+    <a
+      // ref={ref}
+      // initial="hidden"
+      // animate={controls}
+      // variants={fromBottomVariant}
       href={url}
       target="_blank"
       rel="noopener noreferrer"
     >
       <Icon className="icon" title={title} />
-    </motion.a>
+    </a>
   );
 }
