cedcf4b1392986bbd13e526f905895d173cf9b9e
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2022-04-07 13:54:08 +0530

    fixing camelCase issue

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link href="" rel="stylesheet"></link>
         <link
           rel="stylesheet"
