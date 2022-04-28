7d6b7b56b748950bec527e5ab94ae53eda308512
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2022-04-28 22:57:58 +0530

    Disabling Page Transition

--- layout/Layout.js
+++ layout/Layout.js
@@ -2,55 +2,12 @@ import TopNavbar from "../components/TopNavbar";
 import BottomNavbar from "../components/BottomNavbar";
 import ScrollToTopButton from "../components/ScrollToTopButton";
 import Footer from "../components/Footer";
-import { useRouter } from "next/router";
-import { motion, AnimatePresence } from "framer-motion";
 
 export default function Layout({ children }) {
-  const router = useRouter();
-
-  // const pageTransition = {
-  //   hidden: {
-  //     opacity: 0,
-  //     x: 500,
-  //   },
-  //   visible: {
-  //     opacity: 1,
-  //     x: 0,
-  //   },
-  //   exit: {
-  //     opacity: 0,
-  //     x: -500,
-  //   },
-  // };
-
-  const variants = {
-    hidden: { opacity: 1, x: 200, y: 0 },
-    visible: {
-      opacity: 1,
-      x: 0,
-      y: 0,
-      transition: {
-        delayChildren: 1,
-      },
-    },
-    exit: { opacity: 0, x: 0, y: -200 },
-  };
-
   return (
     <>
       <TopNavbar />
-      <AnimatePresence initial={false} exitBeforeEnter>
-        <motion.main
-          key={router.route}
-          variants={variants}
-          initial="hidden"
-          animate="visible"
-          exit="exit"
-          transition={{ type: "linear" }}
-        >
-          {children}
-        </motion.main>
-      </AnimatePresence>
+      <main>{children}</main>
       <Footer />
       <ScrollToTopButton />
       <BottomNavbar />
