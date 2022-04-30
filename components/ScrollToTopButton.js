90e424331906a88171a092b1c6f94d8099e0e767
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2022-04-30 14:45:36 +0530

    hiding it when scrolled to bottom

--- components/ScrollToTopButton.js
+++ components/ScrollToTopButton.js
@@ -4,8 +4,17 @@ import { useEffect, useState } from "react";
 export default function ScrollToTopButton() {
   const [showButton, setShowButton] = useState(false);
 
+  function getScrollPercent() {
+    var h = document.documentElement,
+      b = document.body,
+      st = "scrollTop",
+      sh = "scrollHeight";
+    return parseInt(
+      ((h[st] || b[st]) / ((h[sh] || b[sh]) - h.clientHeight)) * 100
+    );
+  }
   function scrollEvent() {
-    if (window.pageYOffset > 300) {
+    if (window.pageYOffset > 300 && getScrollPercent() < 95) {
       setShowButton(true);
     } else {
       setShowButton(false);
@@ -32,7 +41,7 @@ export default function ScrollToTopButton() {
         <button
           onClick={scrollToTop}
           aria-label="Scroll To Top"
-          className="fixed bottom-[10%] sm:bottom-[7%] right-0 m-5 sm:m-0 z-50"
+          className="fixed bottom-8 right-8 md:bottom-[20px] md:right-[20px]  z-40"
         >
           <IoIosArrowUp className="bg-black dark:bg-gray-200 dark:text-darkPrimary text-white rounded-md shadow-lg text-[50px] md:mr-10" />
         </button>
