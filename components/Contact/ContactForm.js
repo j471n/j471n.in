a3d0f4691b75d158c34df90f8fee3058ceff1f8d
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2022-05-03 14:35:38 +0530

    UPDATE: UI of the Contact form

---
+++ components/Contact/ContactForm.js
@@ -0,0 +1,223 @@
+import { useState } from "react";
+import { AiOutlineLoading } from "react-icons/ai";
+import { ToastContainer, toast } from "react-toastify";
+import { useDarkMode } from "../../context/darkModeContext";
+import emailjs from "emailjs-com";
+import { motion } from "framer-motion";
+import {
+  FadeContainer,
+  mobileNavItemSideways,
+} from "../../content/FramerMotionVariants";
+
+// initial State of the form
+const initialFormState = {
+  to_name: "Jatin Sharma",
+  first_name: "",
+  last_name: "",
+  email: "",
+  subject: "",
+  message: "",
+};
+
+export default function Form() {
+  const [emailInfo, setEmailInfo] = useState(initialFormState);
+  const [loading, setLoading] = useState(false);
+  const { isDarkMode } = useDarkMode();
+
+  function sendEmail(e) {
+    e.preventDefault();
+    setLoading(true);
+
+    emailjs
+      .send(
+        process.env.NEXT_PUBLIC_YOUR_SERVICE_ID,
+        process.env.NEXT_PUBLIC_YOUR_TEMPLATE_ID,
+        emailInfo,
+        process.env.NEXT_PUBLIC_YOUR_USER_ID
+      )
+      .then((res) => {
+        setLoading(false);
+        setEmailInfo(initialFormState);
+        toast.success("Message Sent ✌");
+      })
+      .catch((err) => {
+        console.log(err.text);
+        setLoading(false);
+        toast.error("😢 " + err.text);
+      });
+  }
+
+  function validateForm() {
+    for (const key in emailInfo) {
+      if (emailInfo[key] === "") return false;
+    }
+    return true;
+  }
+
+  function submitFormOnEnter(event) {
+    if ((event.keyCode == 10 || event.keyCode == 13) && event.ctrlKey) {
+      if (validateForm()) {
+        return sendEmail();
+      }
+      toast.error("Looks like you have not filled the form");
+    }
+  }
+
+  return (
+    <>
+      <motion.form
+        initial="hidden"
+        whileInView="visible"
+        variants={FadeContainer}
+        viewport={{ once: true }}
+        className="w-full flex flex-col items-center max-w-xl mx-auto my-10 dark:text-gray-300"
+        onSubmit={sendEmail}
+        onKeyDown={submitFormOnEnter}
+      >
+        {/* First Name And Last Name */}
+        <div className="w-full grid grid-cols-2 gap-6">
+          <motion.div
+            variants={mobileNavItemSideways}
+            className="relative z-0 w-full mb-6 group"
+          >
+            <input
+              type="text"
+              name="first_name"
+              id="floating_first_name"
+              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              placeholder=" "
+              required
+              onChange={(e) =>
+                setEmailInfo({
+                  ...emailInfo,
+                  [e.target.name]: e.target.value,
+                })
+              }
+            />
+            <label
+              htmlFor="floating_first_name"
+              className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            >
+              First name
+            </label>
+          </motion.div>
+          <motion.div
+            variants={mobileNavItemSideways}
+            className="relative z-0 w-full mb-6 group"
+          >
+            <input
+              type="text"
+              name="last_name"
+              id="floating_last_name"
+              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              placeholder=" "
+              required
+              onChange={(e) =>
+                setEmailInfo({
+                  ...emailInfo,
+                  [e.target.name]: e.target.value,
+                })
+              }
+            />
+            <label
+              htmlFor="floating_last_name"
+              className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            >
+              Last name
+            </label>
+          </motion.div>
+        </div>
+        <motion.div
+          variants={mobileNavItemSideways}
+          className="relative z-0 w-full mb-6 group"
+        >
+          <input
+            type="email"
+            name="floating_email"
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            placeholder=" "
+            required
+            onChange={(e) =>
+              setEmailInfo({
+                ...emailInfo,
+                [e.target.name]: e.target.value,
+              })
+            }
+          />
+          <label
+            htmlFor="floating_email"
+            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          >
+            Email address
+          </label>
+        </motion.div>
+        <motion.div
+          variants={mobileNavItemSideways}
+          className="relative z-0 w-full mb-6 group"
+        >
+          <input
+            type="subject"
+            name="floating_subject"
+            id="floating_subject"
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            placeholder=" "
+            required
+            onChange={(e) =>
+              setEmailInfo({
+                ...emailInfo,
+                [e.target.name]: e.target.value,
+              })
+            }
+          />
+          <label
+            htmlFor="floating_subject"
+            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          >
+            Subject
+          </label>
+        </motion.div>
+        <motion.div
+          variants={mobileNavItemSideways}
+          className="relative z-0 w-full mb-6 group"
+        >
+          <textarea
+            name="message"
+            id="floating_message"
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer min-h-[100px] resize-y"
+            placeholder=" "
+            required
+            onChange={(e) =>
+              setEmailInfo({
+                ...emailInfo,
+                [e.target.name]: e.target.value,
+              })
+            }
+          />
+          <label
+            htmlFor="floating_message"
+            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          >
+            Message
+          </label>
+        </motion.div>
+
+        <motion.button
+          variants={mobileNavItemSideways}
+          type="submit"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:max-w-sm px-4 py-3 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          <div className="relative w-full flex items-center justify-center">
+            <p className={loading ? "inline-flex animate-spin mr-3" : "hidden"}>
+              <AiOutlineLoading className="font-bold text-xl" />
+            </p>
+            <p>{loading ? "Sending..." : "Send"}</p>
+          </div>
+        </motion.button>
+      </motion.form>
+      <ToastContainer
+        theme={isDarkMode ? "dark" : "light"}
+        style={{ zIndex: 1000 }}
+      />
+    </>
+  );
+}
