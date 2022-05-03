2ef10442a788ef38bcb962f6ee079fa83677315a
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2022-05-03 14:34:27 +0530

    Changing the styles of QNA

--- components/FAQ/QuestionAndAnswer.js
+++ components/FAQ/QuestionAndAnswer.js
@@ -9,7 +9,7 @@ export default function QuestionAndAnswer({ question, answer }) {
     <div>
       <AnimatedDiv
         variants={popUpFromBottomForText}
-        className="inline-block p-2 sm:p-3 text-white bg-purple-700 rounded-lg"
+        className="inline-block p-2 sm:p-3 text-white bg-slate-500 dark:bg-slate-700 rounded-lg"
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
