ad779969adcf95af412cdd4a82721939c07359a9
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2022-05-12 12:21:15 +0530

    Adding Tip Components

---
+++ components/MDXComponents/Tip.js
@@ -0,0 +1,10 @@
+export default function Tip({ id, children }) {
+  return (
+    <div className="relative w-full px-4  flex gap-2 items-center bg-gray-200 dark:bg-neutral-800 rounded-md">
+      <div className="font-exo font-bold uppercase px-4 py-0 rounded-tl-md absolute top-0 left-0 bg-yellow-400 dark:bg-yellow-500 text-black">
+        Tip {id && `#${id}`}
+      </div>
+      <p className="">{children}</p>
+    </div>
+  );
+}
--- components/MDXComponents/index.js
+++ components/MDXComponents/index.js
@@ -4,5 +4,6 @@ import Figcaption from "./Figcaption";
 import Warning from "./Warning";
 import Danger from "./Danger";
 import CodeTitle from "./CodeTitle";
+import Tip from "./Tip"
 
-export default { Codepen, BlogImage, Figcaption, Warning, Danger, CodeTitle };
+export default { Codepen, BlogImage, Figcaption, Warning, Danger, CodeTitle, Tip };
