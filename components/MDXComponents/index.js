4ba77c1708c7357c0c1aaa18c0d3074f3a48bb76
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2022-05-16 12:04:22 +0530

    ADDED : Copy Code Button

--- components/MDXComponents/index.js
+++ components/MDXComponents/index.js
@@ -4,6 +4,16 @@ import Figcaption from "./Figcaption";
 import Warning from "./Warning";
 import Danger from "./Danger";
 import CodeTitle from "./CodeTitle";
-import Tip from "./Tip"
+import Tip from "./Tip";
+import Pre from "./Pre";
 
-export default { Codepen, BlogImage, Figcaption, Warning, Danger, CodeTitle, Tip };
+export default {
+  Codepen,
+  BlogImage,
+  Figcaption,
+  Warning,
+  Danger,
+  CodeTitle,
+  Tip,
+  pre: Pre,
+};
