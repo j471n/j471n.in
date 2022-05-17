285e1f5498fb9eee6c78d073306db2d0c7c39e3f
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2022-05-17 11:01:25 +0530

    Adding PostCSS rules

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,15 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+  plugins: [
+    [
+      "@fullhuman/postcss-purgecss",
+      {
+        content: [
+          "./pages/**/*.{js,jsx,ts,tsx}",
+          "./components/**/*.{js,jsx,ts,tsx}",
+        ],
+        defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+      },
+    ],
+    "postcss-preset-env",
+  ],
+};
