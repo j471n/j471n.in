3cdf30d4df795bd109c0c39ab5bc1b4cccb9a723
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2022-05-18 09:09:33 +0530

    FIXED: Assign object to a variable before exporting as module defaul

--- components/MDXComponents/index.js
+++ components/MDXComponents/index.js
@@ -8,7 +8,7 @@ import Tip from "./Tip";
 import Pre from "./Pre";
 import Step from "./Step";
 
-export default {
+const MDXComponents = {
   Codepen,
   BlogImage,
   Figcaption,
@@ -19,3 +19,5 @@ export default {
   Step,
   pre: Pre,
 };
+
+export default MDXComponents;
