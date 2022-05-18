bd0b75ee0ff3f42b52ce3b2ab6fcf26dbcce7697
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2022-05-18 11:21:35 +0530

    Updating TIP

--- components/MDXComponents/Tip.js
+++ components/MDXComponents/Tip.js
@@ -1,10 +1,10 @@
 export default function Tip({ id, children }) {
   return (
-    <div className="relative w-full px-4 pt-4 flex gap-2 items-center bg-yellow-100 dark:bg-neutral-800 rounded-md my-5">
-      <div className="font-exo font-bold uppercase px-4 py-0 rounded-tl-md rounded-br-md absolute top-0 left-0 bg-yellow-400 dark:bg-yellow-500 text-black">
+    <div className="relative w-full px-5 pt-4 flex gap-2 items-center bg-yellow-100 dark:bg-neutral-800 rounded-md my-5">
+      <div className="font-barlow font-bold uppercase px-4 py-0 rounded-tl-md rounded-br-md absolute top-0 left-0 bg-yellow-400 dark:bg-yellow-500 text-black">
         Tip {id && `#${id}`}
       </div>
-      <p>{children}</p>
+      {children}
     </div>
   );
 }
