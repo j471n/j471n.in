0d420c318718a6a8ddd4a29eaa5038dcb9582193
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2022-05-18 14:50:00 +0530

    ADDED: Font Setup Optimized

--- pages/_document.js
+++ pages/_document.js
@@ -4,17 +4,61 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
+      
+        {/* Barlow */}
         <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="true"
+          rel="preload"
+          href="/fonts/Barlow/Barlow-400.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
         />
-        <link rel="DNS-prefetch" href="//fonts.googleapis.com" />
         <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Sarina&family=Montserrat:wght@400;500;600;700&family=Inter:wght@400;500;600;700;800&family=Barlow:wght@400;500;600;700;800&display=optional"
+          rel="preload"
+          href="/fonts/Barlow/Barlow-500.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
         />
+        <link
+          rel="preload"
+          href="/fonts/Barlow/Barlow-600.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="/fonts/Barlow/Barlow-700.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="/fonts/Barlow/Barlow-800.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+
+        {/* Inter */}
+        <link
+          rel="preload"
+          href="/fonts/Inter-var.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        {/* Sarina */}
+        <link
+          rel="preload"
+          href="/fonts/Sarina/Sarina-400.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+
         <link
           rel="stylesheet"
           href="https://unpkg.com/@tailwindcss/typography@0.4.x/dist/typography.min.css"
