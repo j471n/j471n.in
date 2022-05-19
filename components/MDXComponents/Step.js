a3219410609a04cef98263b9746785e5b7dff46c
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2022-05-19 12:10:47 +0530

    Style updates for Blog

--- components/MDXComponents/Step.js
+++ components/MDXComponents/Step.js
@@ -4,7 +4,7 @@ export default function Step({ id, children }) {
       <div className="flex items-center justify-center   bg-gray-300  font-bold dark:border-gray-800 rounded-full p-5 w-10 h-10 g-gray-300 ring dark:bg-darkSecondary text-black dark:text-white ">
         {id}
       </div>
-      <div className="text-lg tracking-tight font-bold text-black dark:text-white flex-grow-0 w-fit">
+      <div className="text-lg tracking-tight font-semibold text-black dark:text-white flex-grow-0 w-fit">
         {children}
       </div>
     </div>
