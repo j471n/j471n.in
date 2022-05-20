6722317541ba2c38fad3355c8599cbd60db712a5
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2022-05-20 12:46:51 +0530

    Adding Support of PWA

--- next.config.js
+++ next.config.js
@@ -2,23 +2,35 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
 
-module.exports = withBundleAnalyzer({
-  webpack: true,
-  webpack: (config) => {
-    // Fixes npm packages that depend on `fs` module
-    config.resolve.fallback = { fs: false };
-    return config;
-  },
-  reactStrictMode: true,
-  images: {
-    domains: [
-      "cdn.buymeacoffee.com",
-      "res.cloudinary.com",
-      "imgur.com",
-      "i.imgur.com",
-      "cutt.ly",
-      "activity-graph.herokuapp.com",
-      "i.scdn.co", // images from spotify
-    ],
-  },
-});
+const withPWA = require("next-pwa");
+
+module.exports = withBundleAnalyzer(
+  withPWA({
+    webpack: true,
+    webpack: (config) => {
+      // Fixes npm packages that depend on `fs` module
+      config.resolve.fallback = { fs: false };
+      return config;
+    },
+    reactStrictMode: true,
+    images: {
+      domains: [
+        "cdn.buymeacoffee.com",
+        "res.cloudinary.com",
+        "imgur.com",
+        "i.imgur.com",
+        "cutt.ly",
+        "activity-graph.herokuapp.com",
+        "i.scdn.co", // images from spotify
+      ],
+    },
+
+    // Pwa Setting
+    pwa: {
+      dest: "public",
+      register: true,
+      skipWaiting: true,
+      disable: process.env.NODE_ENV === "development",
+    },
+  })
+);
