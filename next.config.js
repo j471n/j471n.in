28298db72ea775ef67b17bdd2790e08b51869f02
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2022-05-20 13:34:23 +0530

    Updating the Favicon and the 284 Icon

--- next.config.js
+++ next.config.js
@@ -30,7 +30,7 @@ module.exports = withBundleAnalyzer(
       dest: "public",
       register: true,
       skipWaiting: true,
-      disable: process.env.NODE_ENV === "development",
+      // disable: process.env.NODE_ENV === "development",
     },
   })
 );
