49a1f6d7c94d224a9dfc94e2a4d11e06d6a9798c
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2022-05-22 13:35:18 +0530

    Adding Inital Animation when user visit the page

--- layout/Layout.js
+++ layout/Layout.js
@@ -1,15 +1,12 @@
 import TopNavbar from "../components/TopNavbar";
 import ScrollToTopButton from "../components/ScrollToTopButton";
 import Footer from "../components/Footer";
-import { AnimatePresence } from "framer-motion";
 
 export default function Layout({ children }) {
   return (
     <>
       <TopNavbar />
-      <AnimatePresence initial={false}>
-        <main>{children}</main>
-      </AnimatePresence>
+      <main>{children}</main>
       <Footer />
       <ScrollToTopButton />
     </>
