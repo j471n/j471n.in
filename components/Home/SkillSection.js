d9534263e6edf95722df59f02dd5642445395a09
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2022-05-28 11:22:56 +0530

    Adding Hover Animation to the cards

--- components/Home/SkillSection.js
+++ components/Home/SkillSection.js
@@ -11,10 +11,14 @@ import {
   SiGit,
   SiMysql,
   SiFirebase,
-} from "react-icons/si";  
+} from "react-icons/si";
 import { FaReact } from "react-icons/fa";
+import { useDarkMode } from "@context/darkModeContext";
+import * as WindowsAnimation from "@lib/windowsAnimation";
 
 export default function SkillSection({ skills }) {
+  const { isDarkMode } = useDarkMode();
+
   return (
     <section className="mx-5">
       <HomeHeading title="My Top Skills" />
@@ -33,12 +37,16 @@ export default function SkillSection({ skills }) {
               variants={popUp}
               key={index}
               title={skill.name}
-              className="p-4 flex items-center justify-center sm:justify-start gap-4 bg-gray-50 hover:bg-gray-100 dark:bg-darkPrimary hover:dark:bg-darkSecondary border rounded-sm border-gray-300 dark:border-neutral-700 transform origin-center md:origin-top group"
+              onMouseMove={(e) =>
+                WindowsAnimation.showHoverAnimation(e, isDarkMode)
+              }
+              onMouseLeave={(e) => WindowsAnimation.removeHoverAnimation(e)}
+              className="p-4 flex items-center justify-center sm:justify-start gap-4 bg-gray-50 hover:bg-white dark:bg-darkPrimary hover:dark:bg-darkSecondary border rounded-sm border-gray-300 dark:border-neutral-700 transform origin-center md:origin-top group"
             >
-              <div className="relative transition group-hover:scale-110 sm:group-hover:scale-100">
+              <div className="relative transition group-hover:scale-110 sm:group-hover:scale-100 select-none pointer-events-none">
                 <Icon className="w-8 h-8" />
               </div>
-              <p className="hidden sm:inline-flex text-sm md:text-base font-semibold">
+              <p className="hidden sm:inline-flex text-sm md:text-base font-semibold select-none pointer-events-none">
                 {skill.name}
               </p>
             </motion.div>
--- components/Stats/StatsCard.js
+++ components/Stats/StatsCard.js
@@ -1,15 +1,18 @@
 import { motion } from "framer-motion";
 import { opacityVariant, popUp } from "../../content/FramerMotionVariants";
-// import { pop } from "../../content/skillsData";
-
+import * as WindowsAnimation from "@lib/windowsAnimation";
+import { useDarkMode } from "@context/darkModeContext";
 export default function StatsCard({ title, value }) {
+  const { isDarkMode } = useDarkMode();
   return (
     <motion.div
-      className="bg-gray-100 hover:bg-gray-200 dark:bg-darkPrimary hover:dark:bg-darkSecondary flex flex-col justify-center py-4 px-7  rounded select-none border border-gray-300 dark:border-neutral-600 transform origin-center"
+      onMouseMove={(e) => WindowsAnimation.showHoverAnimation(e, isDarkMode)}
+      onMouseLeave={(e) => WindowsAnimation.removeHoverAnimation(e)}  
+      className="bg-gray-50 hover:bg-gray-200 dark:bg-darkPrimary hover:dark:bg-darkSecondary flex flex-col justify-center py-4 px-7  rounded select-none border border-gray-300 dark:border-neutral-600 transform origin-center"
       variants={popUp}
     >
-      <h1 className="text-3xl my-2 font-bold dark:text-white">{value}</h1>
-      <p className="text-base font-medium text-gray-500">{title}</p>
+      <h1 className="text-3xl my-2 font-bold dark:text-white pointer-events-none">{value}</h1>
+      <p className="text-base font-medium text-gray-500 pointer-events-none">{title}</p>
     </motion.div>
   );
 }
