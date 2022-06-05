b107b4ac5e2cbd453ba3d6e44789e91db5e2f6ac
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2022-06-05 12:36:05 +0530

    Updating views api

--- pages/api/views/[slug].js
+++ pages/api/views/[slug].js
@@ -1,31 +1,28 @@
 import { ref } from "@lib/firebase";
 
 export default async (req, res) => {
-  if (process.env.NODE_ENV === "production") {
-    if (req.method === "POST") {
-      const slugRef = ref.child(req.query.slug);
-
-      const { snapshot } = await slugRef.transaction((currentViews) => {
-        if (currentViews === null) {
-          return 1;
-        }
-        return currentViews + 1;
-      });
-
-      return res.status(200).json({
-        total: snapshot.val(),
-      });
-    } else {
-      return res.status(401).json({
-        message: "In Development, Can't add views",
-      });
-    }
-  }
-
+  // Get the total views of the page according to the slug by GET method
   if (req.method === "GET") {
     const snapshot = await ref.child(req.query.slug).once("value");
-    const views = snapshot.val();
+    return res.status(200).json({ views: snapshot.val() });
+  }
+  // check if the app in the production and req method is post only then add the view to the database
+  if (req.method === "POST" && process.env.NODE_ENV === "production") {
+    const slugRef = ref.child(req.query.slug);
 
-    return res.status(200).json({ total: views });
+    const { snapshot } = await slugRef.transaction((currentViews) => {
+      if (currentViews === null) {
+        return 1;
+      }
+      return currentViews + 1;
+    });
+
+    return res.status(200).json({
+      total: snapshot.val(),
+    });
+  } else {
+    return res.status(401).json({
+      message: "In Development, Can't add views",
+    });
   }
 };
