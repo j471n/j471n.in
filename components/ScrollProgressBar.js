15f4cc59eeb99fa62d1808f26d423d6fbd98913c
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2022-06-22 00:14:34 +0530

    Seperating the Progress bar logic

---
+++ components/ScrollProgressBar.js
@@ -0,0 +1,29 @@
+import { useCallback, useEffect, useState } from "react";
+
+export default function ScrollProgressBar() {
+  const [scroll, setScroll] = useState(0);
+
+  const progressBarHandler = useCallback(() => {
+    const totalScroll = document.documentElement.scrollTop;
+    const windowHeight =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight;
+    const scroll = `${totalScroll / windowHeight}`;
+
+    setScroll(scroll);
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener("scroll", progressBarHandler);
+    return () => window.removeEventListener("scroll", progressBarHandler);
+  }, [progressBarHandler]);
+  return (
+    <div
+      className="!fixed left-0 w-full h-1 bg-black dark:bg-white origin-top-left  transform duration-300  top-[44px] sm:top-[63.5px] md:top-[60px]"
+      style={{
+        transform: `scale(${scroll},1)`,
+        zIndex: 100,
+      }}
+    />
+  );
+}
--- pages/blogs/[slug].js
+++ pages/blogs/[slug].js
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from "react";
+import { useEffect } from "react";
 import BlogLayout from "@layout/BlogLayout";
 import { getPostFromSlug, getSlugs } from "@lib/posts";
 import Metadata from "@components/MetaData";
@@ -8,23 +8,6 @@ import { MDXRemote } from "next-mdx-remote";
 import "highlight.js/styles/atom-one-dark.css";
 
 export default function Post({ post }) {
-  const [scroll, setScroll] = useState(0);
-
-  const progressBarHandler = useCallback(() => {
-    const totalScroll = document.documentElement.scrollTop;
-    const windowHeight =
-      document.documentElement.scrollHeight -
-      document.documentElement.clientHeight;
-    const scroll = `${totalScroll / windowHeight}`;
-
-    setScroll(scroll);
-  }, []);
-
-  useEffect(() => {
-    window.addEventListener("scroll", progressBarHandler);
-    return () => window.removeEventListener("scroll", progressBarHandler);
-  }, [progressBarHandler]);
-
   // Adding Views to the firebase database
   useEffect(() => {
     const registerView = () =>
@@ -42,13 +25,7 @@ export default function Post({ post }) {
         description={post.meta.excerpt}
         previewImage={post.meta.image}
       />
-      <div
-        className="!fixed left-0 w-full h-1 bg-black dark:bg-white origin-top-left  transform duration-300  top-[44px] sm:top-[63.5px] md:top-[60px]"
-        style={{
-          transform: `scale(${scroll},1)`,
-          zIndex: 1000,
-        }}
-      />
+
       <BlogLayout post={post}>
         <MDXRemote
           {...post.source}
@@ -62,7 +39,7 @@ export default function Post({ post }) {
 
 export async function getStaticProps({ params }) {
   const { slug } = params;
-const {
+  const {
     content: source,
     meta,
     readingTime,
