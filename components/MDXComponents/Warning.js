35a1a19395536dbf73c35f80bd329c4ff5d77664
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2022-06-22 22:04:44 +0530

    fixing dataicon issue

--- components/MDXComponents/Warning.js
+++ components/MDXComponents/Warning.js
@@ -4,7 +4,7 @@ export default function Warning({ text, title }) {
       <div className="text-2xl font-medium leading-tight mb-2 flex items-center gap-2 text-yellow-700 dark:text-yellow-500">
         <svg
           aria-hidden="true"
-          dataIcon="exclamation-triangle"
+          dataicon="exclamation-triangle"
           className="w-6 h-6 mr-2 fill-current"
           role="img"
           xmlns="http://www.w3.org/2000/svg"
