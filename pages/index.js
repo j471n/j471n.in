b5a1ad006449cde8763400e8561a1e84e8a4790a
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2022-06-23 12:24:30 +0530

    Adding the Circle Animation to the

--- pages/index.js
+++ pages/index.js
@@ -49,7 +49,7 @@ export default function Home({ blogs, skills }) {
           <div className="w-full relative mx-auto flex flex-col items-center gap-10">
             <motion.div
               variants={popUp}
-              className="w-44 h-44 xs:w-52 xs:h-52 flex justify-center items-center border-t-4 border-b-4 border-black dark:border-white rounded-full p-3 "
+              className="relative w-44 h-44 xs:w-52 xs:h-52 flex justify-center items-center rounded-full p-3 before:absolute before:inset-0 before:border-t-4 before:border-b-4 before:border-black before:dark:border-white before:rounded-full before:animate-spin"
             >
               <Image
                 src={homeProfileImage}
