5ac4b16d6ed8857e3c52f5ed7337cebe46afb12a
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2022-06-26 22:44:23 +0530

    Removing Lazy load of Codepen

--- components/MDXComponents/Codepen.js
+++ components/MDXComponents/Codepen.js
@@ -1,13 +1,12 @@
 export default function Codepen({ id }) {
   return (
     <div className="my-3 print:hidden">
-      <h3>Codepen</h3>
       <iframe
         height="600"
         style={{ marginTop: "10px" }}
         className="w-full"
         scrolling="no"
-        src={`https://codepen.io/j471n/embed/preview/${id}`}
+        src={`https://codepen.io/j471n/embed/${id}`}
         frameBorder="no"
         loading="lazy"
         allowFullScreen={true}
