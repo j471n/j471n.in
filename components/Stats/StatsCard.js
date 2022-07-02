0d71fdd41e851f4e03dae9bc340c4af115cbc4de
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2022-07-02 22:10:44 +0530

    UPDATE: Stats Cards UI

--- components/Stats/StatsCard.js
+++ components/Stats/StatsCard.js
@@ -1,18 +1,18 @@
 import { motion } from "framer-motion";
-import { opacityVariant, popUp } from "../../content/FramerMotionVariants";
-import * as WindowsAnimation from "@lib/windowsAnimation";
+import { popUp } from "../../content/FramerMotionVariants";
 import { useDarkMode } from "@context/darkModeContext";
 export default function StatsCard({ title, value }) {
-  const { isDarkMode } = useDarkMode();
   return (
     <motion.div
-      onMouseMove={(e) => WindowsAnimation.showHoverAnimation(e, isDarkMode)}
-      onMouseLeave={(e) => WindowsAnimation.removeHoverAnimation(e)}  
-      className="bg-gray-50 hover:bg-gray-200 dark:bg-darkPrimary hover:dark:bg-darkSecondary flex flex-col justify-center py-4 px-7  rounded select-none border border-gray-300 dark:border-neutral-600 transform origin-center"
+      className="flex-col justify-center py-4 px-7 rounded-md select-none transform origin-center  bg-white dark:bg-darkSecondary shadow dark:shadow-md border border-transparent hover:border-gray-400 dark:hover:border-neutral-600 group"
       variants={popUp}
     >
-      <h1 className="text-3xl my-2 font-bold dark:text-white pointer-events-none">{value}</h1>
-      <p className="text-base font-medium text-gray-500 pointer-events-none">{title}</p>
+      <h1 className="text-3xl my-2 font-bold text-gray-600 dark:text-gray-300 group-hover:text-black  dark:group-hover:text-white">
+        {value}
+      </h1>
+      <p className="text-base font-medium text-gray-500 group-hover:text-black  dark:group-hover:text-white">
+        {title}
+      </p>
     </motion.div>
   );
 }
