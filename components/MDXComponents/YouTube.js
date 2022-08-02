640f88cb0a0997ed33f4bb560ec5fe3a2b804ba8
Jatin Sharma <jatinsharma089659@gmail.com>
2022-08-02 16:45:19 +0530

    Update YouTube.js

--- components/MDXComponents/YouTube.js
+++ components/MDXComponents/YouTube.js
@@ -7,7 +7,7 @@ export default function YouTube({ id }) {
         title="YouTube video player"
         frameBorder={0}
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowFullscreen
+        allowFullScreen
       ></iframe>
     </div>
   );
