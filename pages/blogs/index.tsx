7b040aee1e6e0d81c87dd8acb199a60b0f285410
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2022-12-17 12:46:14 +0530

    Adding shortcut support for search

--- pages/blogs/index.tsx
+++ pages/blogs/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef, KeyboardEvent } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 import {
   FadeContainer,
@@ -17,10 +17,12 @@ import PageTop from "@components/PageTop";
 import MDXContent from "@lib/MDXContent";
 import pageMeta from "@content/meta";
 import { BlogType } from "@lib/types";
+import { CgSearch } from "react-icons/cg";
 
 export default function Blogs({ blogs }: { blogs: BlogType[] }) {
   const [searchValue, setSearchValue] = useState("");
   const [filteredBlogs, setFilteredBlogs] = useState([...blogs]);
+  const searchRef = useRef<HTMLInputElement>(null!);
 
   useEffect(() => {
     setFilteredBlogs(
@@ -29,6 +31,19 @@ export default function Blogs({ blogs }: { blogs: BlogType[] }) {
       )
     );
   }, [searchValue, blogs]);
+
+  function handleAutoSearch(e: any) {
+    if (e.code === "Slash" && e.ctrlKey) {
+      console.log("running");
+      searchRef.current.focus();
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener("keydown", handleAutoSearch);
+
+    return () => document.removeEventListener("keydown", handleAutoSearch);
+  }, []);
 
   return (
     <>
@@ -45,48 +60,26 @@ export default function Blogs({ blogs }: { blogs: BlogType[] }) {
           tech careers. In total, I've written {blogs.length} articles till now.
         </PageTop>
 
-        <AnimatedDiv variants={opacityVariant}>
-          <div className="w-full lg:flex items-center text-sm leading-6 text-slate-400 rounded-md ring-1 ring-slate-900/10 px-2 py-1.5 shadow-sm hover:ring-slate-400 dark:bg-darkSecondary dark:highlight-white/5 dark:hover:bg-darkSecondary/90 mx-auto flex relative bg-white group">
-            <svg
-              width="24"
-              height="24"
-              fill="none"
-              aria-hidden="true"
-              className="mx-3 flex-none"
-            >
-              <path
-                d="m19 19-3.5-3.5"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              ></path>
-              <circle
-                cx="11"
-                cy="11"
-                r="6"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              ></circle>
-            </svg>
-            <input
-              className="px-3 text-slate-400  py-2 w-full  outline-none transition duration-200 bg-transparent font-medium font-inter"
-              type="text"
-              value={searchValue}
-              onChange={(e) => setSearchValue(e.target.value)}
-              placeholder="Search articles..."
-            />
-
-            <button
-              type="button"
-              onClick={() => setSearchValue("")}
-              className="hidden group-hover:inline-flex"
-            >
-              <RiCloseCircleLine className="w-4 h-4 mr-3" />
-            </button>
-          </div>
+        <AnimatedDiv
+          className="relative group text-slate-400 dark:text-gray-300"
+          variants={opacityVariant}
+        >
+          <CgSearch className="ml-3 w-5 h-5 absolute top-[50%] -translate-y-1/2 z-10" />
+          <input
+            ref={searchRef}
+            className="px-12  py-3 w-full  outline-none transition duration-200 bg-transparent font-medium font-inter lg:flex items-center text-sm leading-6 text-slate-400 rounded-md ring-1 ring-slate-900/10 shadow-sm hover:ring-slate-400 dark:bg-darkSecondary dark:highlight-white/5 dark:hover:bg-darkSecondary/90 mx-auto flex relative bg-white group focus:ring-slate-400"
+            type="text"
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+            placeholder="Search articles..."
+          />
+          <button
+            type="button"
+            onClick={() => setSearchValue("")}
+            className="hidden group-focus-within:inline-flex right-3 absolute top-[50%] -translate-y-1/2"
+          >
+            <RiCloseCircleLine className="w-5 h-5 mr-3" />
+          </button>
         </AnimatedDiv>
 
         <section className="relative py-5  flex flex-col gap-2 min-h-[50vh]">
