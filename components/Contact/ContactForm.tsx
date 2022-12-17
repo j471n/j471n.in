034149761c284984f0f172ea75279a2bb83abefe
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2022-12-17 11:51:30 +0530

    Updating Form handling method

--- components/Contact/ContactForm.tsx
+++ components/Contact/ContactForm.tsx
@@ -1,48 +1,63 @@
-import React, { FormEvent, useState } from "react";
+import React from "react";
 import { ToastContainer, toast } from "react-toastify";
-import { useDarkMode } from "../../context/darkModeContext";
+import { useDarkMode } from "@context/darkModeContext";
 import emailjs from "@emailjs/browser";
 import { motion } from "framer-motion";
 import {
   FadeContainer,
   mobileNavItemSideways,
-} from "../../content/FramerMotionVariants";
+} from "@content/FramerMotionVariants";
 import Ripples from "react-ripples";
 import { useRef } from "react";
 import { FormInput } from "@lib/types";
 
-// initial State of the form
-const initialFormState: FormInput = {
-  to_name: "Jatin Sharma",
-  first_name: "",
-  last_name: "",
-  email: "",
-  subject: "",
-  message: "",
-};
-
 export default function Form() {
-  const [emailInfo, setEmailInfo] = useState(initialFormState);
   const { isDarkMode } = useDarkMode();
   const sendButtonRef = useRef<HTMLButtonElement>(null!);
+  const formRef = useRef<HTMLFormElement>(null!);
 
-  function sendEmail(e: FormEvent) {
+  const FailToastId: string = "failed";
+
+  function sendEmail(e: React.SyntheticEvent) {
     e.preventDefault();
+
+    const target = e.target as typeof e.target & {
+      first_name: { value: string };
+      last_name: { value: string };
+      email: { value: string };
+      subject: { value: string };
+      message: { value: string };
+    };
+
+    const emailData = {
+      to_name: "Jatin Sharma",
+      first_name: target.first_name.value.trim(),
+      last_name: target.last_name.value.trim(),
+      email: target.email.value.trim(),
+      subject: target.subject.value.trim(),
+      message: target.message.value.trim(),
+    };
+
+    if (!validateForm(emailData) && !toast.isActive(FailToastId))
+      return toast.error("Looks like you have not filled the form", {
+        toastId: FailToastId,
+      });
+
     // Making submit button disable
     sendButtonRef.current.setAttribute("disabled", "true");
 
-    // Creating a toast
+    // Creating a loading toast
     const toastId = toast.loading("Processing ⌛");
 
     emailjs
       .send(
         process.env.NEXT_PUBLIC_YOUR_SERVICE_ID!,
         process.env.NEXT_PUBLIC_YOUR_TEMPLATE_ID!,
-        emailInfo,
+        emailData!,
         process.env.NEXT_PUBLIC_YOUR_USER_ID
       )
       .then((res) => {
-        setEmailInfo(initialFormState);
+        formRef.current.reset();
         toast.update(toastId, {
           render: "Message Sent ✌",
           type: "success",
@@ -62,36 +77,23 @@ export default function Form() {
       });
   }
 
-  function validateForm() {
-    for (const key in emailInfo) {
-      if (emailInfo[key as keyof FormInput] === "") return false;
+  function validateForm(data: FormInput): boolean {
+    for (const key in data) {
+      if (data[key as keyof FormInput] === "") return false;
     }
     return true;
-  }
-
-  function submitFormOnEnter(event: React.KeyboardEvent) {
-    event;
-    if (
-      (event.code === "ControlLeft" || event.code === "ControlRight") &&
-      event.ctrlKey
-    ) {
-      if (validateForm()) {
-        return sendEmail(event);
-      }
-      toast.error("Looks like you have not filled the form");
-    }
   }
 
   return (
     <>
       <motion.form
+        ref={formRef}
         initial="hidden"
         whileInView="visible"
         variants={FadeContainer}
         viewport={{ once: true }}
         className="w-full flex flex-col items-center max-w-xl mx-auto my-10 dark:text-gray-300"
         onSubmit={sendEmail}
-        onKeyDown={submitFormOnEnter}
       >
         {/* First Name And Last Name */}
         <div className="w-full grid grid-cols-2 gap-6">
@@ -103,20 +105,13 @@ export default function Form() {
               type="text"
               name="first_name"
               id="floating_first_name"
-              className="block py-2 mt-2 px-0 w-full text-sm text-white-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-white focus:outline-none focus:ring-0 focus:border-black peer"
+              className="block py-2 mt-2 px-0 w-full text-sm text-white-900 bg-transparent border-0 border-b-2 border-slate-500 appearance-none dark:text-white dark:border-gray-400 dark:focus:border-white focus:outline-none focus:ring-0 focus:border-black peer"
               placeholder=" "
               required
-              value={emailInfo.first_name}
-              onChange={(e) =>
-                setEmailInfo({
-                  ...emailInfo,
-                  [e.target.name]: e.target.value,
-                })
-              }
             />
             <label
               htmlFor="floating_first_name"
-              className="peer-focus:font-medium absolute text-sm text-slate-400 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-black dark:peer-focus:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+              className="peer-focus:font-medium absolute text-sm text-slate-600 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-black dark:peer-focus:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
             >
               First name
             </label>
@@ -129,20 +124,13 @@ export default function Form() {
               type="text"
               name="last_name"
               id="floating_last_name"
-              className="block py-2 mt-2 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-white focus:outline-none focus:ring-0 focus:border-black peer"
+              className="block py-2 mt-2 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-slate-500 appearance-none dark:text-white dark:border-gray-400 dark:focus:border-white focus:outline-none focus:ring-0 focus:border-black peer"
               placeholder=" "
               required
-              value={emailInfo.last_name}
-              onChange={(e) =>
-                setEmailInfo({
-                  ...emailInfo,
-                  [e.target.name]: e.target.value,
-                })
-              }
             />
             <label
               htmlFor="floating_last_name"
-              className="peer-focus:font-medium absolute text-sm text-slate-400 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-black dark:peer-focus:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+              className="peer-focus:font-medium absolute text-sm text-slate-600 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-black dark:peer-focus:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
             >
               Last name
             </label>
@@ -156,20 +144,13 @@ export default function Form() {
             type="email"
             name="email"
             id="floating_email"
-            className="block py-2 mt-2 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 focus:outline-none focus:ring-0 focus:dark:border-white focus:border-black peer"
+            className="block py-2 mt-2 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-slate-500 appearance-none dark:text-white dark:border-gray-400 focus:outline-none focus:ring-0 focus:dark:border-white focus:border-black peer"
             placeholder=" "
             required
-            value={emailInfo.email}
-            onChange={(e) =>
-              setEmailInfo({
-                ...emailInfo,
-                [e.target.name]: e.target.value,
-              })
-            }
           />
           <label
             htmlFor="floating_email"
-            className="peer-focus:font-medium absolute text-sm text-slate-400 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-black dark:peer-focus:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            className="peer-focus:font-medium absolute text-sm text-slate-600 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-black dark:peer-focus:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
           >
             Email address
           </label>
@@ -182,20 +163,13 @@ export default function Form() {
             type="subject"
             name="subject"
             id="floating_subject"
-            className="block py-2 mt-2 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-white focus:outline-none focus:ring-0 focus:border-black peer"
+            className="block py-2 mt-2 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-slate-500 appearance-none dark:text-white dark:border-gray-400 dark:focus:border-white focus:outline-none focus:ring-0 focus:border-black peer"
             placeholder=" "
             required
-            value={emailInfo.subject}
-            onChange={(e) =>
-              setEmailInfo({
-                ...emailInfo,
-                [e.target.name]: e.target.value,
-              })
-            }
           />
           <label
             htmlFor="floating_subject"
-            className="peer-focus:font-medium absolute text-sm text-slate-400 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-black dark:peer-focus:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            className="peer-focus:font-medium absolute text-sm text-slate-600 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-black dark:peer-focus:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
           >
             Subject
           </label>
@@ -207,20 +181,13 @@ export default function Form() {
           <textarea
             name="message"
             id="floating_message"
-            className="block py-2 mt-2 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-white focus:outline-none focus:ring-0  peer min-h-[100px] resize-y focus:border-black"
+            className="block py-2 mt-2 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-slate-500 appearance-none dark:text-white dark:border-gray-400 dark:focus:border-white focus:outline-none focus:ring-0  peer min-h-[100px] resize-y focus:border-black"
             placeholder=" "
             required
-            value={emailInfo.message}
-            onChange={(e) =>
-              setEmailInfo({
-                ...emailInfo,
-                [e.target.name]: e.target.value,
-              })
-            }
           />
           <label
             htmlFor="floating_message"
-            className="peer-focus:font-medium absolute text-sm text-slate-400 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-black dark:peer-focus:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            className="peer-focus:font-medium absolute text-sm text-slate-600 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-black dark:peer-focus:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
           >
             Message
           </label>
@@ -228,7 +195,7 @@ export default function Form() {
 
         <motion.div
           variants={mobileNavItemSideways}
-          className="w-full sm:max-w-sm rounded-lg overflow-hidden "
+          className="w-full sm:max-w-sm rounded-lg overflow-hidden shadow-lg"
         >
           <Ripples
             className="flex w-full justify-center"
