f53be80ed1ca16ced2b01e251a05078f4abb88aa
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2022-12-17 15:58:10 +0530

    Adding Other SVG Components

---
+++ components/SVG/Zip7.tsx
@@ -0,0 +1,30 @@
+export default function Zip7() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="32"
+      height="32"
+      viewBox="0 0 32 32"
+      fill="none"
+      className="utilities-svg"
+    >
+      <g clipPath="url(#clip0_1332_2)">
+        <path d="M32 4H0V29H32V4Z" fill="none" />
+        <path
+          d="M20.3636 13.5588V15.7647H24.2182L20.3636 21.4265V23.8529H26.9091V21.6471H23.0545L26.9091 15.9853V13.5588H20.3636ZM8 11.3529V13.5588H13.4545L10.1818 16.8676V21.6471H13.0909V16.8676L16 13.9265V11.3529M5.09091 9.14706H18.1818V23.8529H5.09091V9.14706ZM0 4V29H32V4H0ZM2.18182 6.20588H29.8182V26.7941H2.18182"
+          fill="currentColor"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_1332_2">
+          <rect
+            width="32"
+            height="25"
+            fill="white"
+            transform="translate(0 4)"
+          />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
--- components/SVG/index.tsx
+++ components/SVG/index.tsx
@@ -3,12 +3,14 @@ import Flux from "./Flux";
 import MicrosoftToDo from "./MicrosoftToDo";
 import RainDrop from "./RainDrop";
 import ShareX from "./ShareX";
+import Zip7 from "./Zip7";
 
 const SVG = {
-  // Ditto,
-  // Flux,
-  // MicrosoftToDo,
-  // RainDrop,
-  // ShareX,
+  Ditto,
+  Flux,
+  MicrosoftToDo,
+  RainDrop,
+  ShareX,
+  Zip7,
 };
 export default SVG;
