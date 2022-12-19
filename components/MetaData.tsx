8185a5158901569cb17545a3bf96cb91c27db986
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2022-12-19 11:33:53 +0530

    Removing [user-scalable="no"]

--- components/MetaData.tsx
+++ components/MetaData.tsx
@@ -23,10 +23,9 @@ export default function MetaData({
   return (
     <Head>
       <meta charSet="utf-8" />
-      {/* <meta name="viewport" content="width=device-width, initial-scale=1" /> */}
       <meta
         name="viewport"
-        content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+        content="width=device-width,initial-scale=1,minimum-scale=1"
       />
       <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
       <meta name="description" content={description || "Jatin Sharma"} />
