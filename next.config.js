3934c98994370fc20082c2428b9881a715356122
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2022-12-19 14:25:20 +0530

    Updating Config Files

--- next.config.js
+++ next.config.js
@@ -2,52 +2,15 @@
  * @type {import('next').NextConfig}
  */
 
-const withPWA = require("next-pwa");
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+  publicExcludes: ["!resume.pdf"],
+});
 
-// module.exports = withBundleAnalyzer(
-//   withPWA({
-//     webpack: true,
-//     webpack: (config) => {
-//       // Fixes npm packages that depend on `fs` module
-//       config.resolve.fallback = { fs: false };
-//       return config;
-//     },
-//     reactStrictMode: true,
-//     images: {
-//       remotePatterns: [
-//         {
-//           protocol: "https",
-//           hostname: "imgur.com",
-//         },
-//         {
-//           protocol: "https",
-//           hostname: "i.scdn.co",
-//         },
-//       ],
-//       // unoptimized: true,
-//       // domains: [
-//       //   "cdn.buymeacoffee.com",
-//       //   "res.cloudinary.com",
-//       //   "cutt.ly",
-//       //   "activity-graph.herokuapp.com",
-//       //   "i.scdn.co", // images from spotify
-//       //   "images.unsplash.com",
-//       //   "imgur.com",
-//       // ],
-//     },
-
-//     // Pwa Setting
-//     pwa: {
-//       dest: "public",
-//       register: true,
-//       skipWaiting: true,
-//       disable: process.env.NODE_ENV === "development",
-//       publicExcludes: ["!resume.pdf"],
-//     },
-//   })
-// );
-
-module.exports = {
+module.exports = withPWA({
   reactStrictMode: true,
   images: {
     domains: [
@@ -61,4 +24,4 @@ module.exports = {
       "images.unsplash.com",
     ],
   },
-};
+});
