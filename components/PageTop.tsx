a79d67660566cc9fafc6800b724c1711cc8be30e
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2023-01-17 04:12:10 +0530

    Adding containerClass prop

--- components/PageTop.tsx
+++ components/PageTop.tsx
@@ -8,14 +8,18 @@ import AnimatedText from "./FramerMotion/AnimatedText";
 export default function PageTop({
   pageTitle,
   headingClass,
+  containerClass,
   children,
 }: {
   pageTitle: string;
   headingClass?: string;
+  containerClass?: string;
   children?: React.ReactNode;
 }) {
   return (
-    <div className="w-full flex flex-col gap-3 py-5 select-none mb-10">
+    <div
+      className={`w-full flex flex-col gap-3 py-5 select-none mb-10 ${containerClass}`}
+    >
       <AnimatedHeading
         variants={fromLeftVariant}
         className={`text-4xl  md:text-5xl font-bold text-neutral-900 dark:text-neutral-200 ${headingClass}`}
