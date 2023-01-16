f2d52dcf0f57a5f8802589ede4534ce5ee03718c
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2023-01-17 01:15:50 +0530

    Adding Sitemap New Approach

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ import {
 import AnimatedHeading from "@components/FramerMotion/AnimatedHeading";
 import { homeProfileImage } from "@utils/utils";
 import getRSS from "@lib/generateRSS";
-import generateSitemap from "@lib/sitemap";
+// import generateSitemap from "@lib/sitemap";
 import { motion } from "framer-motion";
 import { FiDownload } from "react-icons/fi";
 import MDXContent from "@lib/MDXContent";
@@ -120,7 +120,7 @@ export function HomeHeading({ title }: { title: React.ReactNode | string }) {
 export async function getStaticProps() {
   const blogs = new MDXContent("posts").getAllPosts(3);
   await getRSS();
-  await generateSitemap();
+  // await generateSitemap();
 
   return {
     props: { blogs },
