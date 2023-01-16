8d03fbd72831fd47877840acb7ab5de0ed8fb0c6
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2023-01-17 04:12:36 +0530

    Using Static page Component

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,47 +1,10 @@
-import MDXComponents from "@components/MDXComponents";
-import MetaData from "@components/MetaData";
-import PageTop from "@components/PageTop";
-import Support from "@components/Support";
 import MDXContent from "@lib/MDXContent";
-import { MDXRemote } from "next-mdx-remote";
-import AnimatedDiv from "@components/FramerMotion/AnimatedDiv";
-import { opacityVariant } from "@content/FramerMotionVariants";
 import pageMeta from "@content/meta";
 import { PostType } from "@lib/types";
+import StaticPage from "@components/StaticPage";
 
 export default function About({ about }: { about: PostType }) {
-  return (
-    <>
-      <MetaData
-        title={pageMeta.about.title}
-        description={pageMeta.about.description}
-        previewImage={pageMeta.about.image}
-        keywords={pageMeta.about.keywords}
-      />
-
-      <section className="pageTop">
-        <PageTop pageTitle="About me"></PageTop>
-        <AnimatedDiv
-          variants={opacityVariant}
-          className="prose dark:prose-invert max-w-full"
-        >
-          <MDXRemote
-            {...about.source}
-            frontmatter={{
-              slug: about.meta.slug,
-              excerpt: about.meta.excerpt,
-              title: about.meta.title,
-              date: about.meta.date,
-              keywords: about.meta.keywords,
-              image: about.meta.image,
-            }}
-            components={MDXComponents}
-          />
-        </AnimatedDiv>
-        <Support />
-      </section>
-    </>
-  );
+  return <StaticPage metadata={pageMeta.about} page={about} />;
 }
 
 export async function getStaticProps() {
