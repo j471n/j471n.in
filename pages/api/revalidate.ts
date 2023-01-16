a1760594ed509c278379dd987602d4d2b5cd7cb2
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2023-01-17 02:54:54 +0530

    Creating a new revalidate API route

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -1,17 +1,26 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
+interface ExtendedNextApiRequest extends NextApiRequest {
+  query: {
+    revalidateUrl: string;
+    secret: string;
+  };
+}
 export default async function handler(
-  req: NextApiRequest,
+  req: ExtendedNextApiRequest,
   res: NextApiResponse
 ) {
   // Check for secret to confirm this is a valid request
   if (req.query.secret !== process.env.REVALIDATE_SECRET) {
-    return res.status(401).json({ message: "Invalid token" });
+    return res.status(401).json({
+      message:
+        "Invalid token alert! It looks like you're trying to sneak in without proper authorization. Please present a valid token or face rejection",
+    });
   }
 
   try {
     // Regenerate the projects page
-    await res.revalidate("/projects");
+    await res.revalidate(req.query.revalidateUrl);
     return res.json({ revalidated: true });
   } catch (err) {
     // If there was an error, Next.js will continue
