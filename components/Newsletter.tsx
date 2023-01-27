63b7cee028fd4cce3d5d39f32f25cbfa5c4e41bd
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2023-01-27 15:01:55 +0530

    Using Google Forms for Newsletter

--- components/Newsletter.tsx
+++ components/Newsletter.tsx
@@ -1,9 +1,65 @@
+import { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import { AiOutlineSend } from "react-icons/ai";
+import { useDarkMode } from "@context/darkModeContext";
+
 export default function Newsletter() {
+  const { isDarkMode } = useDarkMode();
+  const [email, setEmail] = useState("");
+
+  async function subscribeNewsLetter(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    try {
+      fetch(process.env.NEXT_PUBLIC_EMAIL_LIST + email, {
+        mode: "no-cors",
+      });
+    } catch (error) {
+      console.error(error);
+    }
+    toast.success("You have been added to my mailing list.");
+    setEmail("");
+  }
+
   return (
-    <iframe
-      className="w-full"
-      src="https://j471n.substack.com/embed"
-      height="250"
-    ></iframe>
+    <>
+      <div className="w-full p-4 font-barlow rounded-lg ring-2 ring-gray-400 bg-white dark:bg-black dark:border-neutral-600 flex flex-col gap-4 mt-10">
+        <h2 className="text-2xl font-bold dark:text-white !my-0">
+          Jatin's Newsletter
+        </h2>
+        <p className="text-gray-600 dark:text-gray-300 font-medium !my-0">
+          I write monthly Tech, Web Development and chrome extension that will
+          improve your productivity. Trust me, I won't spam you.
+        </p>
+
+        <form className="relative w-full" onSubmit={subscribeNewsLetter}>
+          <input
+            className="px-4 py-2.5 rounded-lg text-lg bg-gray-200 dark:bg-darkSecondary outline-none border-0 w-full placeholder:text-gray-700 dark:placeholder:text-gray-400 dark:text-gray-300"
+            type="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="example@email.com"
+            required={true}
+          />
+
+          <button
+            className="absolute right-0 top-0 bottom-0 px-4 m-[3px] bg-white dark:text-white dark:bg-neutral-600/40   rounded-md font-medium font-inter transform duration-200 active:scale-90 select-none"
+            type="submit"
+          >
+            <div className="relative flex items-center gap-2 !my-0">
+              <AiOutlineSend className="text-xl" />
+              <p className="hidden sm:inline-flex !my-0">Subscribe</p>
+            </div>
+          </button>
+        </form>
+      </div>
+
+      <ToastContainer
+        theme={isDarkMode ? "dark" : "light"}
+        style={{ zIndex: 1000 }}
+        autoClose={3000}
+      />
+    </>
   );
 }
