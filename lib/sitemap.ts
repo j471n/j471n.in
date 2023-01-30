b703cd3b4df6529715d095a0336c3da967150d82
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2023-01-31 02:27:39 +0530

    Adding Snippets to sitemap

--- lib/sitemap.ts
+++ lib/sitemap.ts
@@ -5,10 +5,13 @@ export default async function generate() {
   const pages = await globby([
     "pages/*.tsx",
     "posts/*.mdx",
+    "snippets/*.mdx",
     "!pages/_*.tsx",
     "!pages/api",
     "!pages/404.tsx",
   ]);
+
+  console.log(pages);
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
@@ -17,6 +20,7 @@ export default async function generate() {
             const path = page
               .replace("pages", "")
               .replace("posts", "/blogs")
+              .replace("snippets", "/snippets")
               .replace(".tsx", "")
               .replace(".mdx", "");
             const route = path === "/index" ? "" : path;
@@ -30,6 +34,8 @@ export default async function generate() {
           .join("")}
     </urlset>
     `;
+
+  console.log(sitemap);
 
   // eslint-disable-next-line no-sync
   writeFileSync("public/sitemap.xml", sitemap);
