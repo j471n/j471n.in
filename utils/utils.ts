dc8b7f89006c37ba65f1a4dac0644792f66acd5f
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2023-01-31 02:28:03 +0530

    Adding Ts and Supabase icons links

--- utils/utils.ts
+++ utils/utils.ts
@@ -20,4 +20,6 @@ export const snippetsImages: { [key: string]: string } = {
   css: "https://imgur.com/ArD8JIg.png",
   js: "https://imgur.com/lFKi8mB.png",
   react: "https://imgur.com/m2jv6MK.png",
+  ts: "https://imgur.com/Ux6L5Uh.png",
+  supabase: "https://imgur.com/xgNKVQa.png",
 };
