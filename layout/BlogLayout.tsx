8f728669c368336a1d3fbebcf3e9d55c2595d46e
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2023-02-10 00:40:05 +0530

    fix: Removing Margin 9 from <p> tag

--- layout/BlogLayout.tsx
+++ layout/BlogLayout.tsx
@@ -58,7 +58,7 @@ export default function BlogLayout({
         </h1>
 
         <div className="flex items-center !w-full text-gray-700 dark:text-gray-300">
-          <div className="flex items-center gap-2 w-full">
+          <div className="flex items-center w-full gap-2">
             <Image
               alt="Jatin Sharma"
               height={30}
@@ -66,7 +66,7 @@ export default function BlogLayout({
               src={homeProfileImage}
               className="rounded-full !m-0"
             />
-            <div className="flex flex-col text-xs sm:text-sm sm:flex-row sm:justify-between w-full">
+            <div className="flex flex-col w-full text-xs sm:text-sm sm:flex-row sm:justify-between">
               <p className="flex items-center gap-2 font-medium !my-0">
                 <span>Jatin Sharma</span>
                 <span>•</span>
@@ -87,9 +87,9 @@ export default function BlogLayout({
               title="Edit on Github"
               target="_blank"
               rel="noopener noreferrer"
-              className="transition active:scale-75 select-none"
+              className="transition select-none active:scale-75"
             >
-              <RxPencil2 className="w-7 h-7 text-gray-700 dark:text-gray-300 stroke-slate-300 " />
+              <RxPencil2 className="text-gray-700 w-7 h-7 dark:text-gray-300 stroke-slate-300 " />
             </Link>
             <button
               title="Save for Later"
@@ -111,26 +111,26 @@ export default function BlogLayout({
 
         <AnimatedDiv
           variants={opacityVariant}
-          className="blog-container max-w-full prose-sm sm:prose-base prose-pre:bg-white prose-pre:shadow-lg prose-img:mx-auto prose-p:!my-0 dark:prose-pre:shadow-black/80 dark:prose-pre:bg-darkSecondary prose-pre:saturate-150 dark:prose-pre:saturate-100 marker:text-black dark:marker:text-white"
+          className="max-w-full prose-sm blog-container sm:prose-base prose-pre:bg-white prose-img:mx-auto prose-img:rounded-md dark:prose-pre:bg-darkSecondary prose-pre:saturate-150 dark:prose-pre:saturate-100 marker:text-black dark:marker:text-white"
         >
           {children}
         </AnimatedDiv>
         <Newsletter />
-        <div className="w-full flex flex-col items-center gap-4 my-10 print:hidden">
+        <div className="flex flex-col items-center w-full gap-4 my-10 print:hidden">
           <h3
             style={{ margin: "0" }}
-            className="font-semibold text-xl dark:text-white"
+            className="text-xl font-semibold dark:text-white"
           >
             Share on Social Media:
           </h3>
           <ShareOnSocialMedia
-            className="flex gap-2 items-center flex-wrap w-fit"
+            className="flex flex-wrap items-center gap-2 w-fit"
             title={post.meta.title}
             url={currentURL}
             summary={post.meta.excerpt}
             cover_image={post.meta.image}
           >
-            <div className="bg-gray-700 text-white p-2 rounded-full cursor-pointer">
+            <div className="p-2 text-white bg-gray-700 rounded-full cursor-pointer">
               <FiPrinter className="w-4 h-4" onClick={() => window.print()} />
             </div>
           </ShareOnSocialMedia>
