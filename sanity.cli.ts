080926e4d18a0f71f77a5e8c7c90d83073613fdc
Jatin Sharma <jatinsharma089659@gmail.com>
2023-07-04 02:54:36 +0530

    moving sanity cli file

---
+++ sanity.cli.ts
@@ -0,0 +1,8 @@
+import { defineCliConfig } from "@sanity/cli";
+
+export default defineCliConfig({
+  api: {
+    projectId: "5ec749u0",
+    dataset: "production",
+  },
+});
--- sanity/sanity.cli.ts
+++
@@ -1,8 +0,0 @@
-import {defineCliConfig} from 'sanity/cli'
-
-export default defineCliConfig({
-  api: {
-    projectId: '5ec749u0',
-    dataset: 'production',
-  },
-})
