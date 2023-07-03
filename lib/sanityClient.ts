dcc344a786814e9d2e1b8cff89d8f645321222fe
Jatin Sharma <jatinsharma089659@gmail.com>
2023-07-04 02:49:38 +0530

    adding api version

--- lib/sanityClient.ts
+++ lib/sanityClient.ts
@@ -4,6 +4,7 @@ const client = createClient({
   projectId: process.env.SANITY_PROJECT_ID, // you can find this in sanity.json
   dataset: "production", // or the name you chose in step 1
   useCdn: true, // `false` if you want to ensure fresh data
+  apiVersion: "2021-08-31",
 });
 
 export default client;
