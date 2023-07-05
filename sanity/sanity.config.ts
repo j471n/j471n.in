4fba2e10b90ed85f5a8c44d6ef400a3ccb9e73df
Jatin Sharma <jatinsharma089659@gmail.com>
2023-07-05 23:32:23 +0530

    removing mdx & setting config

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -1,4 +1,4 @@
-import {defineConfig} from 'sanity'
+import {defineConfig} from 'sanity/lib/exports'
 import {deskTool} from 'sanity/desk'
 import {schemaTypes} from './schemas'
 import {visionTool} from '@sanity/vision'
