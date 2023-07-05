5f022c6c16dab5be5d1e6adb149cf53e6a3f58eb
Jatin Sharma <jatinsharma089659@gmail.com>
2023-07-06 02:57:42 +0530

    using server response

--- pages/api/stats/devto.ts
+++ pages/api/stats/devto.ts
@@ -1,14 +1,11 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextRequest, NextResponse } from "next/server";
 import { allFollowers, allPosts } from "../../../lib/devto";
 
 export const config = {
   runtime: "edge", // this is a pre-requisite
 };
 
-export default async function handler(
-  _req: NextApiRequest,
-  res: NextApiResponse
-) {
+export default async function handler(_req: NextRequest) {
   const followers = await allFollowers();
 
   const posts = await allPosts();
@@ -23,16 +20,19 @@ export default async function handler(
     totalComments += post.comments_count;
   });
 
-  res.setHeader(
-    "Cache-Control",
-    "public, s-maxage=86400, stale-while-revalidate=43200"
+  return NextResponse.json(
+    {
+      followers: followers,
+      likes: totalLikes,
+      views: totalViews,
+      comments: totalComments,
+      posts: posts.length,
+    },
+    {
+      headers: {
+        "Cache-Control": "public, s-maxage=86400, stale-while-revalidate=43200",
+      },
+      status: 200,
+    }
   );
-
-  return res.status(200).json({
-    followers: followers,
-    likes: totalLikes,
-    views: totalViews,
-    comments: totalComments,
-    posts: posts.length,
-  });
 }
