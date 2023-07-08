083dbce0764cfaed69e1b539bab547ce8e6d82f1
Jatin Sharma <jatinsharma089659@gmail.com>
2023-07-08 22:25:58 +0530

    adding loading in the charts

--- components/GitHubActivityGraph.tsx
+++ components/GitHubActivityGraph.tsx
@@ -48,45 +48,49 @@ export default function GitHubActivityGraph() {
           last 31 days.
         </AnimatedText>
         <ResponsiveContainer width="100%" height={300}>
-          <AreaChart
-            width={730}
-            height={250}
-            data={githubActivity?.contributions}
-            margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
-          >
-            <defs>
-              <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-                <stop
-                  offset="5%"
-                  stopColor={isDarkMode ? "#26a64160" : "#26a641"}
-                  stopOpacity={0.8}
-                />
-                <stop
-                  offset="95%"
-                  stopColor={isDarkMode ? "#26a64160" : "#26a641"}
-                  stopOpacity={0}
-                />
-              </linearGradient>
-            </defs>
-            <XAxis dataKey="shortDate" />
-            <YAxis />
-            <CartesianGrid
-              strokeDasharray="2 3"
-              stroke={isDarkMode ? "#ffffff20" : "#00000020"}
-            />
-            <Tooltip content={<ContributionsToolTip />} />
-            <Area
-              dot
-              activeDot
-              strokeWidth={3}
-              type="monotone"
-              dataKey="contributionCount"
-              aria-label="count"
-              stroke={isDarkMode ? "#26a641" : "#216e39"}
-              fillOpacity={1}
-              fill="url(#colorUv)"
-            />
-          </AreaChart>
+          {githubActivity?.contributions ? (
+            <AreaChart
+              width={730}
+              height={250}
+              data={githubActivity?.contributions}
+              margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+            >
+              <defs>
+                <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                  <stop
+                    offset="5%"
+                    stopColor={isDarkMode ? "#26a64160" : "#26a641"}
+                    stopOpacity={0.8}
+                  />
+                  <stop
+                    offset="95%"
+                    stopColor={isDarkMode ? "#26a64160" : "#26a641"}
+                    stopOpacity={0}
+                  />
+                </linearGradient>
+              </defs>
+              <XAxis dataKey="shortDate" />
+              <YAxis />
+              <CartesianGrid
+                strokeDasharray="2 3"
+                stroke={isDarkMode ? "#ffffff20" : "#00000020"}
+              />
+              <Tooltip content={<ContributionsToolTip />} />
+              <Area
+                dot
+                activeDot
+                strokeWidth={3}
+                type="monotone"
+                dataKey="contributionCount"
+                aria-label="count"
+                stroke={isDarkMode ? "#26a641" : "#216e39"}
+                fillOpacity={1}
+                fill="url(#colorUv)"
+              />
+            </AreaChart>
+          ) : (
+            <LoadingAreaChart />
+          )}
         </ResponsiveContainer>
       </div>
       <div className="font-barlow mb-10 max-w-full">
@@ -104,22 +108,26 @@ export default function GitHubActivityGraph() {
           contributions made on each day of the week.
         </AnimatedText>
         <ResponsiveContainer width="100%" height={300}>
-          <BarChart
-            width={730}
-            height={250}
-            data={githubActivity?.contributionCountByDayOfWeek}
-            margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
-          >
-            <CartesianGrid
-              strokeDasharray="2 3"
-              stroke={isDarkMode ? "#ffffff20" : "#00000020"}
-            />
-            <XAxis dataKey="day" />
-            <YAxis />
+          {githubActivity?.contributionCountByDayOfWeek ? (
+            <BarChart
+              width={730}
+              height={250}
+              data={githubActivity?.contributionCountByDayOfWeek}
+              margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+            >
+              <CartesianGrid
+                strokeDasharray="2 3"
+                stroke={isDarkMode ? "#ffffff20" : "#00000020"}
+              />
+              <XAxis dataKey="day" />
+              <YAxis />
 
-            <Tooltip content={<ContributionCountByDayOfWeekToolTip />} />
-            <Bar dataKey="count" fill="#26a641" />
-          </BarChart>
+              <Tooltip content={<ContributionCountByDayOfWeekToolTip />} />
+              <Bar dataKey="count" fill="#26a641" />
+            </BarChart>
+          ) : (
+            <LoadingBarChart />
+          )}
         </ResponsiveContainer>
       </div>
     </>
@@ -166,3 +174,122 @@ const ContributionCountByDayOfWeekToolTip = ({
 
   return null;
 };
+
+function LoadingBarChart() {
+  const { isDarkMode } = useDarkMode();
+
+  const barGraphLoadingData = [
+    { day: "Monday", count: 3 },
+    { day: "Tuesday", count: 5 },
+    { day: "Wednesday", count: 7 },
+    { day: "Thursday", count: 9 },
+    { day: "Friday", count: 11 },
+    { day: "Saturday", count: 13 },
+    { day: "Sunday", count: 15 },
+  ];
+
+  return (
+    <div className="pointer-events-none relative">
+      <div className="grid place-items-center font-semibold text-base sm:text-lg absolute inset-0 z-1">
+        Loading Data...
+      </div>
+      <BarChart
+        width={730}
+        height={250}
+        data={barGraphLoadingData}
+        margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+        style={{
+          opacity: "0.25",
+        }}
+      >
+        <CartesianGrid
+          strokeDasharray="2 3"
+          stroke={isDarkMode ? "#ffffff20" : "#00000020"}
+        />
+        <XAxis dataKey="day" />
+        <YAxis />
+
+        <Tooltip
+          cursor={{ fill: "transparent" }}
+          content={<ContributionCountByDayOfWeekToolTip />}
+        />
+        <Bar dataKey="count" fill={isDarkMode ? "#404040" : "#ababab"} />
+      </BarChart>
+    </div>
+  );
+}
+
+function LoadingAreaChart() {
+  const { isDarkMode } = useDarkMode();
+
+  const areaChartLoadingData = [
+    { shortDate: "09", contributionCount: 3 },
+    { shortDate: "10", contributionCount: 5 },
+    { shortDate: "11", contributionCount: 15 },
+    { shortDate: "12", contributionCount: 9 },
+    { shortDate: "13", contributionCount: 4 },
+    { shortDate: "14", contributionCount: 13 },
+    { shortDate: "15", contributionCount: 5 },
+    { shortDate: "16", contributionCount: 4 },
+    { shortDate: "17", contributionCount: 9 },
+    { shortDate: "18", contributionCount: 2 },
+    { shortDate: "19", contributionCount: 5 },
+    { shortDate: "21", contributionCount: 6 },
+    { shortDate: "22", contributionCount: 7 },
+    { shortDate: "23", contributionCount: 3 },
+    { shortDate: "24", contributionCount: 21 },
+    { shortDate: "25", contributionCount: 4 },
+    { shortDate: "26", contributionCount: 9 },
+    { shortDate: "27", contributionCount: 2 },
+  ];
+
+  return (
+    <div className="pointer-events-none relative">
+      <div className="grid place-items-center font-semibold text-base sm:text-lg absolute inset-0 z-1">
+        Loading Data...
+      </div>
+      <AreaChart
+        width={730}
+        height={250}
+        data={areaChartLoadingData}
+        margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+        style={{
+          opacity: "0.25",
+        }}
+      >
+        <defs>
+          <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+            <stop
+              offset="5%"
+              stopColor={isDarkMode ? "#404040" : "#ababab"}
+              stopOpacity={0.8}
+            />
+            <stop
+              offset="95%"
+              stopColor={isDarkMode ? "#404040" : "#ababab"}
+              stopOpacity={0}
+            />
+          </linearGradient>
+        </defs>
+        <XAxis dataKey="shortDate" />
+        <YAxis />
+        <CartesianGrid
+          strokeDasharray="2 3"
+          stroke={isDarkMode ? "#ffffff20" : "#00000020"}
+        />
+        <Tooltip content={<ContributionsToolTip />} />
+        <Area
+          dot
+          activeDot
+          strokeWidth={3}
+          type="monotone"
+          dataKey="contributionCount"
+          aria-label="count"
+          stroke={isDarkMode ? "#404040" : "#ababab"}
+          fillOpacity={1}
+          fill="url(#colorUv)"
+        />
+      </AreaChart>
+    </div>
+  );
+}
