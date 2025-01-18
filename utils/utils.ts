a1ba2a7b456ff2d00c9b1dac452c93158ae9d675
Jatin Sharma <jatinsharma089659@gmail.com>
2025-01-18 09:49:15 +0530

    Updating the Profile Image

--- utils/utils.ts
+++ utils/utils.ts
@@ -1,6 +1,6 @@
 export const DEFAULT_IMAGE_URL: string = "https://imgur.com/5dYYce8.png";
 export const AvatarImage: string = "https://imgur.com/VAXEwKT.png";
-export const homeProfileImage: string = "https://imgur.com/VAXEwKT.png";
+export const homeProfileImage: string = "https://i.imgur.com/fuciSoZ.jpeg";
 
 export const navigationRoutes: string[] = [
   "home",
